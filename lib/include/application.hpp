64b81bcf24333734e2980eacd76b6ad88902ca38
voxelmesh <voxelmesh@proton.me>
2022-06-21 08:57:11 -0700

    Added application class

---
+++ lib/include/application.hpp
@@ -0,0 +1,23 @@
+//
+// Created by december on 6/21/22.
+//
+
+#pragma once
+
+namespace vkx {
+    struct AppConfig {
+        std::uint32_t windowWidth;
+        std::uint32_t windowHeight;
+    };
+
+    class App {
+    public:
+        App() = default;
+
+        App(const AppConfig &configuration);
+
+        virtual ~Application() = default;
+
+        virtual void run();
+    };
+}
