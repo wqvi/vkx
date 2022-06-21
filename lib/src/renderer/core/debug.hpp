903e531f873fc1463afb7f59b47daaaf31fcd0d8
voxelmesh <voxelmesh@proton.me>
2022-06-21 08:36:35 -0700

    Formatted debug files

--- lib/src/renderer/core/debug.hpp
+++ lib/src/renderer/core/debug.hpp
@@ -2,6 +2,8 @@
 
 #ifdef DEBUG
 extern "C" {
-  VkBool32 vkDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, VkDebugUtilsMessengerCallbackDataEXT const *pCallbackData, void *pUserData);
+VkBool32
+vkDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+                VkDebugUtilsMessengerCallbackDataEXT const *pCallbackData, void *pUserData);
 }
 #endif
