43f4fb8626479c261146c9c9b43b0d6febdf110b
voxelmesh <voxelmesh@proton.me>
2022-06-21 08:40:50 -0700

    Formatted RendererContext

--- lib/include/renderer/core/context.hpp
+++ lib/include/renderer/core/context.hpp
@@ -3,25 +3,26 @@
 #include <renderer/core/renderer_types.hpp>
 #include <renderer/core/profile.hpp>
 
-namespace vkx
-{
-    class RendererContext
-    {
+namespace vkx {
+    class RendererContext {
     public:
-        RendererContext(Profile const &profile);
+        explicit RendererContext(const Profile &profile);
 
-        [[nodiscard]] std::unordered_map<std::uint32_t, vk::PhysicalDevice> getPhysicalDevices(vk::UniqueSurfaceKHR const &surface) const;
+        [[nodiscard]] std::unordered_map<std::uint32_t, vk::PhysicalDevice>
+        getPhysicalDevices(const vk::UniqueSurfaceKHR &surface) const;
 
     protected:
         vk::UniqueInstance instance;
-        
+
     private:
-        Profile profile;
+        const Profile profile;
 
-        static std::vector<char const*> getWindowExtensions();
+        static std::vector<const char *> getWindowExtensions();
 
-        static vk::UniqueInstance createInstance(vk::InstanceCreateInfo const &instanceCreateInfo);
+        static vk::UniqueInstance createInstance(const vk::InstanceCreateInfo &instanceCreateInfo);
 
-        static std::uint32_t ratePhysicalDevice(vk::PhysicalDevice const &physicalDevice, vk::UniqueSurfaceKHR const &surface, Profile const &profile);
+        static std::uint32_t
+        ratePhysicalDevice(const vk::PhysicalDevice &physicalDevice, const vk::UniqueSurfaceKHR &surface,
+                           const Profile &profile);
     };
 }
--- lib/src/renderer/core/context.cpp
+++ lib/src/renderer/core/context.cpp
@@ -4,47 +4,45 @@
 #include <renderer/core/swapchain_info.hpp>
 #include "debug.hpp"
 
-namespace vkx
-{
-    RendererContext::RendererContext(Profile const &profile)
-        : profile(profile)
-    {
-        constexpr static vk::ApplicationInfo const applicationInfo{
-            "Voxel Game",             // pApplicationName
-            VK_MAKE_VERSION(1, 0, 0), // applicationVersion
-            "Voxel Engine",           // engineVersion
-            VK_MAKE_VERSION(1, 0, 0), // engineVersion
-            VK_API_VERSION_1_0        // apiVersion
+namespace vkx {
+    RendererContext::RendererContext(const Profile &profile)
+            : profile(profile) {
+        constexpr static const vk::ApplicationInfo applicationInfo{
+                "Voxel Game",             // pApplicationName
+                VK_MAKE_VERSION(1, 0, 0), // applicationVersion
+                "Voxel Engine",           // engineVersion
+                VK_MAKE_VERSION(1, 0, 0), // engineVersion
+                VK_API_VERSION_1_0        // apiVersion
         };
 
         auto instanceExtensions = getWindowExtensions();
 
-        vk::InstanceCreateInfo instanceCreateInfo{
-            {},                // flags
-            &applicationInfo,  // applicationInfo
-            profile.layers,    // pEnabledLayerNames
-            instanceExtensions // pEnabledExtensionNames
+        const vk::InstanceCreateInfo instanceCreateInfo{
+                {},                // flags
+                &applicationInfo,  // applicationInfo
+                profile.layers,    // pEnabledLayerNames
+                instanceExtensions // pEnabledExtensionNames
         };
 
         instance = createInstance(instanceCreateInfo);
     }
 
-    std::unordered_map<std::uint32_t, vk::PhysicalDevice> RendererContext::getPhysicalDevices(vk::UniqueSurfaceKHR const &surface) const
-    {
+    std::unordered_map<std::uint32_t, vk::PhysicalDevice>
+    RendererContext::getPhysicalDevices(const vk::UniqueSurfaceKHR &surface) const {
         auto physicalDevices = instance->enumeratePhysicalDevices();
         std::unordered_map<std::uint32_t, vk::PhysicalDevice> ratedPhysicalDevices;
-        std::ranges::transform(physicalDevices, std::inserter(ratedPhysicalDevices, ratedPhysicalDevices.begin()), [&surface, &profile = this->profile](auto const &physicalDevice)
-        {
-            return std::make_pair(ratePhysicalDevice(physicalDevice, surface, profile), physicalDevice);
-        });
+        std::ranges::transform(physicalDevices, std::inserter(ratedPhysicalDevices, ratedPhysicalDevices.begin()),
+                               [&surface, &profile = this->profile](const auto &physicalDevice) {
+                                   return std::make_pair(ratePhysicalDevice(physicalDevice, surface, profile),
+                                                         physicalDevice);
+                               });
         return ratedPhysicalDevices;
     }
 
-    std::vector<char const *> RendererContext::getWindowExtensions()
-    {
+    std::vector<char const *> RendererContext::getWindowExtensions() {
         std::uint32_t glfwExtensionCount = 0;
-        char const **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-        std::vector<char const *> instanceExtensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::vector<const char *> instanceExtensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
 
 #ifdef DEBUG
         instanceExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
@@ -52,49 +50,48 @@ namespace vkx
         return instanceExtensions;
     }
 
-    vk::UniqueInstance RendererContext::createInstance(vk::InstanceCreateInfo const &instanceCreateInfo)
-    {
+    vk::UniqueInstance RendererContext::createInstance(const vk::InstanceCreateInfo &instanceCreateInfo) {
 #ifdef DEBUG
-        auto messageSeverity = vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError;
-        auto messageType = vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance;
+        auto messageSeverity =
+                vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning | vk::DebugUtilsMessageSeverityFlagBitsEXT::eError;
+        auto messageType =
+                vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation |
+                vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance;
 
         vk::DebugUtilsMessengerCreateInfoEXT debugUtilsMessengerCreateInfo{
-            {},              // flags
-            messageSeverity, // messageSeverity
-            messageType,     // messageType
-            vkDebugCallback, // pfnUserCallback
-            nullptr          // pUserData
+                {},              // flags
+                messageSeverity, // messageSeverity
+                messageType,     // messageType
+                vkDebugCallback, // pfnUserCallback
+                nullptr          // pUserData
         };
 
         vk::StructureChain structureChain{
-            instanceCreateInfo,
-            debugUtilsMessengerCreateInfo};
+                instanceCreateInfo,
+                debugUtilsMessengerCreateInfo};
         return vk::createInstanceUnique(structureChain.get<vk::InstanceCreateInfo>());
 #else
         return vk::createInstanceUnique(instanceCreateInfo);
 #endif
     }
 
-    std::uint32_t RendererContext::ratePhysicalDevice(vk::PhysicalDevice const &physicalDevice, vk::UniqueSurfaceKHR const &surface, Profile const &profile)
-    {
+    std::uint32_t
+    RendererContext::ratePhysicalDevice(const vk::PhysicalDevice &physicalDevice, const vk::UniqueSurfaceKHR &surface,
+                                        const Profile &profile) {
         std::uint32_t rating = 0;
-        if (profile.validateExts(physicalDevice))
-        {
+        if (profile.validateExts(physicalDevice)) {
             rating++;
         }
 
-        if (QueueConfig indices{physicalDevice, surface}; indices.isComplete())
-        {
+        if (QueueConfig indices{physicalDevice, surface}; indices.isComplete()) {
             rating++;
         }
 
-        if (SwapchainInfo info{physicalDevice, surface}; info.isComplete())
-        {
+        if (SwapchainInfo info{physicalDevice, surface}; info.isComplete()) {
             rating++;
         }
 
-        if (physicalDevice.getFeatures().samplerAnisotropy)
-        {
+        if (physicalDevice.getFeatures().samplerAnisotropy) {
             rating++;
         }
 
