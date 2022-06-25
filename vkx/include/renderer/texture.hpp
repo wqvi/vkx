0fd6b417332166e16f50609d68244947092c5ec3
voxelmesh <voxelmesh@proton.me>
2022-06-24 17:09:22 -0700

    Emptied contents into model.hpp

--- vkx/include/renderer/texture.hpp
+++ vkx/include/renderer/texture.hpp
@@ -4,17 +4,4 @@
 
 namespace vkx
 {
-    class Texture
-    {
-    public:
-        Texture() = default;
-
-        Texture(std::string const &file, Device const &device);
-
-        [[nodiscard]] vk::WriteDescriptorSet createWriteDescriptorSet(vk::DescriptorSet const &descriptorSet, std::uint32_t dstBinding) const;
-    private:
-        Image image;
-        vk::UniqueImageView view;
-        vk::UniqueSampler sampler;
-    };
 }
