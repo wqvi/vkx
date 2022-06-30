bb34c703df63440e2f02b32308832358d33713dd
voxelmesh <voxelmesh@proton.me>
2022-06-30 09:53:46 -0700

    Moved SDLWindow class here

--- vkx/include/window.hpp
+++ vkx/include/window.hpp
@@ -26,4 +26,47 @@ namespace vkx {
 
         SDL_Window *internalHandle;
     };
+
+    // A SDL window wrapper class
+    // It has unique ownership over the pointer
+    class SDLWindow {
+        // Helper class that default initializes thus making the construction of the
+        // managed pointer much simpler looking
+        struct SDL_Deleter {
+            void operator()(SDL_Window *ptr) const {
+                SDL_DestroyWindow(ptr);
+            }
+        };
+
+    public:
+        using EventFun = void (*)(SDL_WindowEvent *);
+
+        SDLWindow() = default;
+
+        explicit SDLWindow(const char *title, int width, int height);
+
+        void show() const noexcept;
+
+        void hide() const noexcept;
+
+        [[nodiscard]] std::pair<int, int> getSize() const noexcept;
+
+        [[nodiscard]] int getWidth() const noexcept;
+
+        [[nodiscard]] int getHeight() const noexcept;
+
+        void pollWindowEvent(const SDL_WindowEvent &event);
+
+        void handleResizeEvent(const SDL_WindowEvent &event);
+
+        [[nodiscard]] bool isResized() const noexcept;
+
+        [[nodiscard]] vk::UniqueSurfaceKHR createSurface(const vk::UniqueInstance &instance) const;
+
+    private:
+        bool framebufferResized = false;
+
+        std::unique_ptr<SDL_Window, SDL_Deleter> window;
+    };
+
 }
--- vkx/src/window.cpp
+++ vkx/src/window.cpp
@@ -2,6 +2,8 @@
 
 #include <SDL2/SDL_vulkan.h>
 
+#include <vkx_exceptions.hpp>
+
 namespace vkx {
     Window::Window(const char *title, int width, int height) {
         int sdlErrorCode = SDL_Init(SDL_INIT_EVERYTHING);
@@ -55,4 +57,79 @@ namespace vkx {
         SDL_Vulkan_GetDrawableSize(internalHandle, &width, &height);
         return {static_cast<std::uint32_t>(width), static_cast<std::uint32_t>(height)};
     }
+
+
+}
+
+vkx::SDLWindow::SDLWindow(const char *title, int width, int height) {
+    SDL_Window *sdlWindow = SDL_CreateWindow(title,
+                                             SDL_WINDOWPOS_UNDEFINED,
+                                             SDL_WINDOWPOS_UNDEFINED,
+                                             width,
+                                             height,
+                                             SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
+    if (sdlWindow == nullptr) {
+//        throw vkx::SDLError();
+    }
+
+    window = std::unique_ptr<SDL_Window, SDL_Deleter>(sdlWindow);
+}
+
+void vkx::SDLWindow::show() const noexcept {
+    SDL_ShowWindow(window.get());
+}
+
+void vkx::SDLWindow::hide() const noexcept {
+    SDL_HideWindow(window.get());
+}
+
+std::pair<int, int> vkx::SDLWindow::getSize() const noexcept {
+    int width;
+    int height;
+
+    SDL_GetWindowSize(window.get(), &width, &height);
+
+    return std::make_pair(width, height);
+}
+
+int vkx::SDLWindow::getWidth() const noexcept {
+    int width;
+
+    SDL_GetWindowSize(window.get(), &width, nullptr);
+
+    return width;
+}
+
+int vkx::SDLWindow::getHeight() const noexcept {
+    int height;
+
+    SDL_GetWindowSize(window.get(), nullptr, &height);
+
+    return height;
+}
+
+void vkx::SDLWindow::pollWindowEvent(const SDL_WindowEvent &event) {
+    switch (event.type) {
+        case SDL_WINDOWEVENT_RESIZED:
+            handleResizeEvent(event);
+            break;
+        default:
+            return;
+    }
+}
+
+void vkx::SDLWindow::handleResizeEvent(const SDL_WindowEvent &event) {
+    framebufferResized = true;
+}
+
+bool vkx::SDLWindow::isResized() const noexcept {
+    return framebufferResized;
+}
+
+vk::UniqueSurfaceKHR vkx::SDLWindow::createSurface(const vk::UniqueInstance &instance) const {
+    VkSurfaceKHR surface = nullptr;
+    if (SDL_Vulkan_CreateSurface(window.get(), *instance, &surface) != SDL_TRUE) {
+//        throw vkx::VulkanError("Failure to create VkSurfaceKHR via the SDL2 API.");
+    }
+    return vk::UniqueSurfaceKHR(surface, *instance);
 }
