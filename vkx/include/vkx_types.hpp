7299f8b700079850038e23bbcda56f12aff5e322
voxelmesh <voxelmesh@proton.me>
2022-06-30 09:52:27 -0700

    Created type forward declaration header

---
+++ vkx/include/vkx_types.hpp
@@ -0,0 +1,17 @@
+//
+// Created by december on 6/30/22.
+//
+
+#pragma once
+
+namespace vkx {
+    struct ApplicationConfig;
+
+    class Application;
+
+    class SDLWindow;
+
+    class Scene;
+
+    class SDLError;
+}
