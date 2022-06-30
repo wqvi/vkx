8ff549a9aae0660ba4f544c4cdbfe174f93c7a65
voxelmesh <voxelmesh@proton.me>
2022-06-30 09:53:05 -0700

    Moved scene to scene.hpp and exceptions to vkx_exception.hpp

--- vkx/include/application.hpp
+++ vkx/include/application.hpp
@@ -8,74 +8,11 @@
 #include <scene.hpp>
 
 namespace vkx {
-    class Scene;
-
     struct ApplicationConfig {
         const char *title = "VKX Application";
         int windowWidth = 640;
         int windowHeight = 360;
         glm::f32 fov = 70.0f;
-    };
-
-    class SDLError : public std::exception {
-    public:
-        [[nodiscard]] const char *what() const noexcept override;
-    };
-
-    class VulkanError : public std::exception {
-    public:
-        explicit VulkanError(const char *message);
-
-        explicit VulkanError(vk::Result result);
-
-        [[nodiscard]] const char *what() const noexcept override;
-
-    private:
-        std::string message;
-    };
-
-    // A SDL window wrapper class
-    // It has unique ownership over the pointer
-    class SDLWindow {
-        // Helper class that default initializes thus making the construction of the
-        // managed pointer much simpler looking
-        // This is honestly syntactic constructor sugar in the source files
-        struct SDL_Deleter {
-            void operator()(SDL_Window *ptr) const {
-                SDL_DestroyWindow(ptr);
-            }
-        };
-
-    public:
-        using EventFun = void (*)(SDL_WindowEvent *);
-
-        SDLWindow() = default;
-
-        // Creates a window with the information from the config
-        explicit SDLWindow(const ApplicationConfig &config);
-
-        void show() const noexcept;
-
-        void hide() const noexcept;
-
-        [[nodiscard]] std::pair<int, int> getSize() const noexcept;
-
-        [[nodiscard]] int getWidth() const noexcept;
-
-        [[nodiscard]] int getHeight() const noexcept;
-
-        void pollWindowEvent(const SDL_WindowEvent &event);
-
-        void handleResizeEvent(const SDL_WindowEvent &event);
-
-        [[nodiscard]] bool isResized() const noexcept;
-
-        [[nodiscard]] vk::UniqueSurfaceKHR createSurface(const vk::UniqueInstance &instance) const;
-
-    private:
-        bool framebufferResized = false;
-
-        std::unique_ptr<SDL_Window, SDL_Deleter> window;
     };
 
     class Application {
@@ -104,26 +41,5 @@ namespace vkx {
         bool isRunning = false;
 
         RendererBase renderer;
-    };
-
-    class Scene {
-    public:
-        Scene() = default;
-
-        virtual ~Scene() = default;
-
-        virtual void init(const vkx::ApplicationConfig &config, const vkx::Application *data) = 0;
-
-        virtual void update() = 0;
-
-        virtual void physics(float deltaTime) = 0;
-
-        virtual void destroy() noexcept = 0;
-
-        virtual void onKeyPress() = 0;
-
-        virtual void onKeyRelease() = 0;
-
-        virtual void onMouseMove() = 0;
     };
 }
--- vkx/src/application.cpp
+++ vkx/src/application.cpp
@@ -4,100 +4,13 @@
 
 #include <application.hpp>
 
-const char *vkx::SDLError::what() const noexcept {
-    return SDL_GetError();
-}
-
-vkx::VulkanError::VulkanError(const char *message)
-        : message(message) {}
-
-vkx::VulkanError::VulkanError(vk::Result result)
-        : message(vk::to_string(result)) {}
-
-const char *vkx::VulkanError::what() const noexcept {
-    return message.c_str();
-}
-
-vkx::SDLWindow::SDLWindow(const ApplicationConfig &config) {
-    SDL_Window *sdlWindow = SDL_CreateWindow(config.title,
-                                             SDL_WINDOWPOS_UNDEFINED,
-                                             SDL_WINDOWPOS_UNDEFINED,
-                                             config.windowWidth,
-                                             config.windowHeight,
-                                             SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN);
-    if (sdlWindow == nullptr) {
-        throw vkx::SDLError();
-    }
-
-    window = std::unique_ptr<SDL_Window, SDL_Deleter>(sdlWindow);
-}
-
-void vkx::SDLWindow::show() const noexcept {
-    SDL_ShowWindow(window.get());
-}
-
-void vkx::SDLWindow::hide() const noexcept {
-    SDL_HideWindow(window.get());
-}
-
-std::pair<int, int> vkx::SDLWindow::getSize() const noexcept {
-    int width;
-    int height;
-
-    SDL_GetWindowSize(window.get(), &width, &height);
-
-    return std::make_pair(width, height);
-}
-
-int vkx::SDLWindow::getWidth() const noexcept {
-    int width;
-
-    SDL_GetWindowSize(window.get(), &width, nullptr);
-
-    return width;
-}
-
-int vkx::SDLWindow::getHeight() const noexcept {
-    int height;
-
-    SDL_GetWindowSize(window.get(), nullptr, &height);
-
-    return height;
-}
-
-void vkx::SDLWindow::pollWindowEvent(const SDL_WindowEvent &event) {
-    switch (event.type) {
-        case SDL_WINDOWEVENT_RESIZED:
-            handleResizeEvent(event);
-            break;
-        default:
-            return;
-    }
-}
-
-void vkx::SDLWindow::handleResizeEvent(const SDL_WindowEvent &event) {
-    framebufferResized = true;
-}
-
-bool vkx::SDLWindow::isResized() const noexcept {
-    return framebufferResized;
-}
-
-vk::UniqueSurfaceKHR vkx::SDLWindow::createSurface(const vk::UniqueInstance &instance) const {
-    VkSurfaceKHR surface = nullptr;
-    if (SDL_Vulkan_CreateSurface(window.get(), *instance, &surface) != SDL_TRUE) {
-        throw vkx::VulkanError("Failure to create VkSurfaceKHR via the SDL2 API.");
-    }
-    return vk::UniqueSurfaceKHR(surface, *instance);
-}
-
 vkx::Application::Application(const vkx::ApplicationConfig &config) {
     int sdlErrorCode = SDL_Init(SDL_INIT_EVERYTHING);
     if (sdlErrorCode < 0) {
         throw std::system_error(std::error_code(sdlErrorCode, std::generic_category()), SDL_GetError());
     }
 
-    window = SDLWindow(config);
+//    window = vkx::SDLWindow(config);
 
     sdlErrorCode = SDL_ShowCursor(SDL_DISABLE);
     if (sdlErrorCode < 0) {
