119178631de6db4a774c7f7123ad5ff62553bd3e
december <voxelmesh@proton.me>
2022-07-03 08:31:46 -0700

    Removed old window class

--- vkx/include/window.hpp
+++ vkx/include/window.hpp
@@ -5,29 +5,6 @@
 #include <scene.hpp>
 
 namespace vkx {
-    class Window {
-    public:
-        Window() = delete;
-
-        Window(const char *title, int width, int height);
-
-        ~Window();
-
-        [[maybe_unused]]
-        void show() const;
-
-        [[maybe_unused]]
-        void hide() const;
-
-        [[nodiscard]]
-        vk::UniqueSurfaceKHR createSurface(vk::UniqueInstance const &instance) const;
-
-        [[nodiscard]]
-        std::pair<std::uint32_t, std::uint32_t> getSize() const;
-
-        SDL_Window *internalHandle;
-    };
-
     // A SDL window wrapper class
     // It has unique ownership over the pointer
     class SDLWindow {
--- vkx/src/window.cpp
+++ vkx/src/window.cpp
@@ -5,58 +5,6 @@
 #include <vkx_exceptions.hpp>
 
 namespace vkx {
-    Window::Window(const char *title, int width, int height) {
-        int sdlErrorCode = SDL_Init(SDL_INIT_EVERYTHING);
-        if (sdlErrorCode < 0) {
-            throw std::system_error(std::error_code(sdlErrorCode, std::generic_category()), SDL_GetError());
-        }
-
-        Uint32 flags = SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN;
-
-        internalHandle = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, flags);
-
-        if (internalHandle == nullptr) {
-            throw std::runtime_error(SDL_GetError());
-        }
-
-        sdlErrorCode = SDL_ShowCursor(SDL_DISABLE);
-        if (sdlErrorCode < 0) {
-            throw std::system_error(std::error_code(sdlErrorCode, std::generic_category()), SDL_GetError());
-        }
-
-        sdlErrorCode = SDL_SetRelativeMouseMode(SDL_TRUE);
-        if (sdlErrorCode < 0) {
-            throw std::system_error(std::error_code(sdlErrorCode, std::generic_category()), SDL_GetError());
-        }
-    }
-
-    Window::~Window() {
-        SDL_DestroyWindow(internalHandle);
-        SDL_Quit();
-    }
-
-    void Window::show() const {
-        SDL_ShowWindow(internalHandle);
-    }
-
-    void Window::hide() const {
-        SDL_HideWindow(internalHandle);
-    }
-
-    vk::UniqueSurfaceKHR Window::createSurface(vk::UniqueInstance const &instance) const {
-        VkSurfaceKHR cSurface = nullptr;
-        if (SDL_Vulkan_CreateSurface(internalHandle, *instance, &cSurface) == SDL_FALSE) {
-            throw std::runtime_error(SDL_GetError());
-        }
-        return vk::UniqueSurfaceKHR(cSurface, *instance);
-    }
-
-    std::pair<std::uint32_t, std::uint32_t> Window::getSize() const {
-        int width = 0;
-        int height = 0;
-        SDL_Vulkan_GetDrawableSize(internalHandle, &width, &height);
-        return {static_cast<std::uint32_t>(width), static_cast<std::uint32_t>(height)};
-    }
 
 
 }
