c37307c5a8c859ef9fabe6894d5a1e52d68c5c45
december <voxelmesh@proton.me>
2022-07-05 09:26:52 -0700

    Moved everything into the vkx namespace. Along with that added a way to stop movement

--- vkx/include/camera.hpp
+++ vkx/include/camera.hpp
@@ -1,26 +1,34 @@
 #pragma once
 
-constexpr static const glm::vec3 NORTH(0.0f, 0.0f, 1.0f);
-constexpr static const glm::vec3 EAST(1.0f, 0.0f, 0.0f);
-constexpr static const glm::vec3 SOUTH(0.0f, 0.0f, -1.0f);
-constexpr static const glm::vec3 WEST(-1.0f, 0.0f, 0.0f);
-constexpr static const glm::vec3 UP(0.0f, 1.0f, 0.0f);
-constexpr static const glm::vec3 DOWN(0.0f, -1.0f, 0.0f);
+#include <node.hpp>
 
 /* TO BE FIXED BUT DON'T TOUCH FOR NOW! */
-class Camera {
-public:
-	Camera() = default;
-	explicit Camera(glm::vec3 const &position);
-	void updateMouse(glm::vec2 const &relative);
-	void updateKey(int key);
-	[[nodiscard]] glm::mat4 viewMatrix() const;
+namespace vkx {
+    constexpr static const glm::vec3 NORTH = glm::vec3{0.0f, 0.0f, 1.0f};
+    constexpr static const glm::vec3 EAST = glm::vec3{1.0f, 0.0f, 0.0f};
+    constexpr static const glm::vec3 SOUTH = glm::vec3{0.0f, 0.0f, -1.0f};
+    constexpr static const glm::vec3 WEST = glm::vec3{-1.0f, 0.0f, 0.0f};
+    constexpr static const glm::vec3 UP = glm::vec3{0.0f, 1.0f, 0.0f};
+    constexpr static const glm::vec3 DOWN = glm::vec3{0.0f, -1.0f, 0.0f};
 
-	glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::quat yawOrientation = glm::quat(0.0f, 0.0f, 0.0f, 1.0f);
-	glm::quat pitchOrientation = glm::quat(0.0f, 0.0f, 0.0f, 1.0f);
-	glm::vec2 sensitivity = glm::vec2(0.1f, 0.1f);
-	glm::vec2 rotation = glm::vec2(0.0f, 0.0f);
-	glm::vec3 direction = glm::vec3(0.0f, 0.0f, 0.0f);
-};
+    class Camera : public Node {
+    public:
+        Camera() = default;
+
+        explicit Camera(glm::vec3 const &position);
+
+        void updateMouse(glm::vec2 const &relative);
+
+        void updateKey(int key);
+
+        [[nodiscard]] glm::mat4 viewMatrix() const;
+
+        glm::vec3 position = glm::vec3{0.0f, 0.0f, 0.0f};
+        glm::vec3 velocity = glm::vec3{0.0f, 0.0f, 0.0f};
+        glm::quat yawOrientation = glm::quat{0.0f, 0.0f, 0.0f, 1.0f};
+        glm::quat pitchOrientation = glm::quat{0.0f, 0.0f, 0.0f, 1.0f};
+        glm::vec2 sensitivity = glm::vec2{0.1f, 0.1f};
+        glm::vec2 rotation = glm::vec2{0.0f, 0.0f};
+        glm::vec3 direction = glm::vec3{0.0f, 0.0f, 0.0f};
+    };
+}
