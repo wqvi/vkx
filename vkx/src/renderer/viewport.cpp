1fff7d4dd54040066455fa3d483828d29486a099
december <voxelmesh@proton.me>
2022-07-05 11:33:26 -0700

    Fixed matrix bug

--- vkx/src/renderer/viewport.cpp
+++ vkx/src/renderer/viewport.cpp
@@ -13,7 +13,9 @@ vkx::Viewport::Viewport(std::uint32_t fov, Sint32 width, Sint32 height)
                           0.1f,
                           100.0f)),
           width(width),
-          height(height) {}
+          height(height) {
+    projection[1][1] *= -1.0f;
+}
 
 vkx::Viewport::operator const glm::mat4 &() const {
     return projection;
@@ -30,6 +32,7 @@ void vkx::Viewport::setFOV(std::uint32_t newFOV) noexcept {
             static_cast<glm::f32>(width) / static_cast<glm::f32>(height),
             0.1f,
             100.0f);
+    projection[1][1] *= -1.0f;
 }
 
 void vkx::Viewport::setSize(Sint32 newWidth, Sint32 newHeight) {
@@ -40,4 +43,5 @@ void vkx::Viewport::setSize(Sint32 newWidth, Sint32 newHeight) {
             static_cast<glm::f32>(width) / static_cast<glm::f32>(height),
             0.1f,
             100.0f);
+    projection[1][1] *= -1.0f;
 }
