d894807856ed40ce516b8f321e2d4cafc3a69356
december <voxelmesh@proton.me>
2022-07-05 12:29:09 -0700

    Working on better abstracted buffer

--- vkx/include/renderer/buffer.hpp
+++ vkx/include/renderer/buffer.hpp
@@ -3,21 +3,85 @@
 #include <renderer/core/allocable.hpp>
 #include <renderer/core/device.hpp>
 
-namespace vkx
-{
-  class Buffer : public Allocable<vk::Buffer>
-  {
-  public:
-    Buffer() = default;
+namespace vkx {
+    class Buffer : public Allocable<vk::Buffer> {
+    public:
+        Buffer() = default;
 
-    explicit Buffer(std::vector<Vertex> const &vertices, Device const &device);
+        explicit Buffer(std::vector<Vertex> const &vertices, Device const &device);
 
-    explicit Buffer(std::vector<std::uint32_t> const &indices, Device const &device);
+        explicit Buffer(std::vector<std::uint32_t> const &indices, Device const &device);
 
-    explicit Buffer(std::size_t size, vk::BufferUsageFlags const &usage, Device const &device);
+        explicit Buffer(std::size_t size, vk::BufferUsageFlags const &usage, Device const &device);
 
-  private:
-    template <class T>
-    explicit Buffer(T const *data, std::size_t size, vk::BufferUsageFlags const &usage, Device const &device);
-  };
+    private:
+        template<class T>
+        explicit Buffer(T const *data, std::size_t size, vk::BufferUsageFlags const &usage, Device const &device);
+    };
+
+    class BufferBase {
+    public:
+        [[nodiscard("You most likely are causing an error if you discard this vertex buffer memory")]]
+        vk::UniqueDeviceMemory const &getMemory() const;
+
+    protected:
+        BufferBase() = default;
+
+        template<class T>
+        explicit BufferBase(T const *data,
+                            std::size_t size,
+                            Device const &device);
+
+    private:
+        vk::UniqueBuffer buffer;
+        vk::UniqueDeviceMemory memory;
+    };
+
+    class VertexBuffer : public BufferBase {
+    public:
+        explicit VertexBuffer(std::vector<Vertex> const &vertices, Device const &device);
+    };
+
+    class IndexBuffer : public BufferBase {
+    public:
+        explicit IndexBuffer(std::vector<std::uint32_t> const &indices, Device const &device);
+    };
+}
+
+template<class T>
+vkx::BufferBase::BufferBase(T const *data, std::size_t size, vkx::Device const &device) {
+    // Create cpu side staging buffers
+    auto stagingBuffer = device.createBufferUnique(
+            size,
+            vk::BufferUsageFlagBits::eTransferSrc);
+
+    auto stagingBufferMemory = device.allocateMemoryUnique(
+            stagingBuffer,
+            vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+
+    // Populate cpu side staging buffers
+    auto *mappedMemory = static_cast<T *>(device->mapMemory(
+            *stagingBufferMemory,
+            0,
+            size,
+            {}));
+    std::memcpy(mappedMemory, data, size);
+    device->unmapMemory(*stagingBufferMemory);
+
+    // Make gpu sided buffer usage flags based on type of template parameter
+    vk::BufferUsageFlagBits usage = vk::BufferUsageFlagBits::eVertexBuffer;
+    if (std::is_same<T, std::uint32_t>::value) {
+        usage = vk::BufferUsageFlagBits::eIndexBuffer;
+    }
+
+    // Allocate gpu size buffer
+    buffer = device.createBufferUnique(
+            size,
+            vk::BufferUsageFlagBits::eTransferDst | usage);
+    memory = device.allocateMemoryUnique(
+            buffer,
+            vk::MemoryPropertyFlagBits::eDeviceLocal);
+
+    // Copy cpu side buffer to gpu side buffer
+    device.copyBuffer(*stagingBuffer, *buffer, size);
 }
--- vkx/src/renderer/buffer.cpp
+++ vkx/src/renderer/buffer.cpp
@@ -2,33 +2,43 @@
 
 #include <renderer/core/device.hpp>
 
-namespace vkx
-{
-  Buffer::Buffer(std::vector<Vertex> const &vertices, Device const &device)
-      : Buffer(vertices.data(), vertices.size() * sizeof(Vertex), vk::BufferUsageFlagBits::eVertexBuffer, device) {}
+namespace vkx {
+    Buffer::Buffer(std::vector<Vertex> const &vertices, Device const &device)
+            : Buffer(vertices.data(), vertices.size() * sizeof(Vertex), vk::BufferUsageFlagBits::eVertexBuffer,
+                     device) {}
 
-  Buffer::Buffer(std::vector<std::uint32_t> const &indices, Device const &device)
-      : Buffer(indices.data(), indices.size() * sizeof(std::uint32_t), vk::BufferUsageFlagBits::eIndexBuffer, device) {}
+    Buffer::Buffer(std::vector<std::uint32_t> const &indices, Device const &device)
+            : Buffer(indices.data(), indices.size() * sizeof(std::uint32_t), vk::BufferUsageFlagBits::eIndexBuffer,
+                     device) {}
 
-  Buffer::Buffer(std::size_t size, vk::BufferUsageFlags const &usage, Device const &device)
-  {
-    obj = device.createBufferUnique(size, vk::BufferUsageFlagBits::eTransferDst | usage);
-    memory = device.allocateMemoryUnique(obj, vk::MemoryPropertyFlagBits::eDeviceLocal);
-  }
+    Buffer::Buffer(std::size_t size, vk::BufferUsageFlags const &usage, Device const &device) {
+        obj = device.createBufferUnique(size, vk::BufferUsageFlagBits::eTransferDst | usage);
+        memory = device.allocateMemoryUnique(obj, vk::MemoryPropertyFlagBits::eDeviceLocal);
+    }
 
-  template <class T>
-  Buffer::Buffer(T const *data, std::size_t size, vk::BufferUsageFlags const &usage, Device const &device)
-  {
-    auto stagingBuffer = device.createBufferUnique(size, vk::BufferUsageFlagBits::eTransferSrc);
-    auto stagingBufferMemory = device.allocateMemoryUnique(stagingBuffer, vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+    template<class T>
+    Buffer::Buffer(T const *data, std::size_t size, vk::BufferUsageFlags const &usage, Device const &device) {
+        auto stagingBuffer = device.createBufferUnique(size, vk::BufferUsageFlagBits::eTransferSrc);
+        auto stagingBufferMemory = device.allocateMemoryUnique(stagingBuffer, vk::MemoryPropertyFlagBits::eHostVisible |
+                                                                              vk::MemoryPropertyFlagBits::eHostCoherent);
 
-    auto *mappedMemory = static_cast<T *>(device->mapMemory(*stagingBufferMemory, 0, size, {}));
-    std::memcpy(mappedMemory, data, size);
-    device->unmapMemory(*stagingBufferMemory);
+        auto *mappedMemory = static_cast<T *>(device->mapMemory(*stagingBufferMemory, 0, size, {}));
+        std::memcpy(mappedMemory, data, size);
+        device->unmapMemory(*stagingBufferMemory);
 
-    obj = device.createBufferUnique(size, vk::BufferUsageFlagBits::eTransferDst | usage);
-    memory = device.allocateMemoryUnique(obj, vk::MemoryPropertyFlagBits::eDeviceLocal);
+        obj = device.createBufferUnique(size, vk::BufferUsageFlagBits::eTransferDst | usage);
+        memory = device.allocateMemoryUnique(obj, vk::MemoryPropertyFlagBits::eDeviceLocal);
 
-    device.copyBuffer(*stagingBuffer, *obj, size);
-  }
+        device.copyBuffer(*stagingBuffer, *obj, size);
+    }
 }
+
+vk::UniqueDeviceMemory const &vkx::BufferBase::getMemory() const {
+    return memory;
+}
+
+vkx::VertexBuffer::VertexBuffer(std::vector<Vertex> const &vertices, Device const &device)
+        : BufferBase(vertices.data(), vertices.size() * sizeof(Vertex), device) {}
+
+vkx::IndexBuffer::IndexBuffer(std::vector<std::uint32_t> const &indices, Device const &device)
+        : BufferBase(indices.data(), indices.size() * sizeof(Vertex), device) {}
