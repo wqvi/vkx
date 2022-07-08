41b94a4ed205b408c2c0a97a822f51d7c47ac5b5
december <voxelmesh@proton.me>
2022-07-08 10:32:24 -0700

    Added new type, vkx::PhysicalDevice source and header to project

---
+++ vkx/include/renderer/core/physical_device.hpp
@@ -0,0 +1,14 @@
+//
+// Created by december on 7/8/22.
+//
+
+#pragma once
+
+#include <renderer/core/renderer_types.hpp>
+
+namespace vkx {
+    class PhysicalDevice {
+    public:
+
+    };
+}
---
+++ vkx/src/renderer/core/physical_device.cpp
@@ -0,0 +1,5 @@
+//
+// Created by december on 7/8/22.
+//
+
+#include <renderer/core/physical_device.hpp>
