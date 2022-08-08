a528db69f155282113fdcf8209acf7f89cbda2c9
voxelmesh <voxelmesh@proton.me>
2022-08-08 09:42:51 -0700

    Cleaned up voxels a bit

--- src/voxels/voxels.cpp
+++ src/voxels/voxels.cpp
@@ -2,173 +2,123 @@
 
 #include <iostream>
 
-namespace vkx {
-    VoxelChunk::VoxelChunk(const glm::vec3 &worldPosition, std::int32_t width, std::int32_t height, std::int32_t depth)
-            : voxels(width, height, depth), worldPosition(worldPosition) {
-        for (std::int32_t x = 0; x < width; x++) {
-            for (std::int32_t y = 0; y < height; y++) {
-                for (std::int32_t z = 0; z < depth / 2; z++) {
-                    voxels.set(x, y, z, Voxel{VoxelType::Stone});
-                }
-            }
-        }
+vkx::VoxelChunk::VoxelChunk(const glm::vec3& worldPosition, std::int32_t size)
+    : worldPosition(worldPosition), voxels(size, size, size), axisLimit(size) {
 
-        for (std::int32_t i = 0; i < width; i++) {
-            voxels.set(i, 2, 2, Voxel{VoxelType::Stone});
-        }
+	for (std::int32_t x = 0; x < size; x++) {
+		for (std::int32_t y = 0; y < size; y++) {
+			for (std::int32_t z = 0; z < size / 2; z++) {
+				voxels.set(x, y, z, Voxel{VoxelType::Stone});
+			}
+		}
+	}
 
-        for (std::int32_t i = 0; i < depth; i++) {
-            voxels.set(0, 0, i, Voxel{VoxelType::Stone});
-        }
+	for (std::int32_t i = 0; i < size; i++) {
+		voxels.set(i, 2, 2, Voxel{VoxelType::Stone});
+	}
 
-        for (std::int32_t i = 0; i < depth; i++) {
-            for (std::int32_t j = height - height / 2; j < height; j++) {
-                voxels.set(0, j, i, Voxel{VoxelType::Stone});
-            }
-        }
+	for (std::int32_t i = 0; i < size; i++) {
+		voxels.set(0, 0, i, Voxel{VoxelType::Stone});
+	}
 
-        for (std::int32_t i = 0; i < width; i++) {
-            voxels.set(i, 0, 0, Voxel{VoxelType::Air, false});
-        }
+	for (std::int32_t i = 0; i < size; i++) {
+		for (std::int32_t j = size - size / 2; j < size; j++) {
+			voxels.set(0, j, i, Voxel{VoxelType::Stone});
+		}
+	}
 
-        voxels.set(1, 1, 1, Voxel(VoxelType::Stone));
-    }
+	for (std::int32_t i = 0; i < size; i++) {
+		voxels.set(i, 0, 0, Voxel{VoxelType::Air, false});
+	}
 
-    VoxelChunk::VoxelChunk(const glm::vec3 &worldPosition, std::int32_t size)
-            : VoxelChunk(worldPosition, size, size, size) {}
+	voxels.set(1, 1, 1, Voxel(VoxelType::Stone));
+}
 
-    void VoxelChunk::greedy() {
-        for (std::int32_t Axis = 0; Axis < 3; ++Axis) {
-            auto const Axis1 = (Axis + 1) % 3;
-            auto const Axis2 = (Axis + 2) % 3;
+void vkx::VoxelChunk::greedy() {
+	for (std::int32_t axis = 0; axis < 3; axis++) {
+		const auto axis1 = (axis + 1) % 3;
+		const auto axis2 = (axis + 2) % 3;
 
-            auto const MainAxisLimit = voxels.getDimensionSize(Axis);
-            auto const Axis1Limit = voxels.getDimensionSize(Axis1);
-            auto const Axis2Limit = voxels.getDimensionSize(Axis2);
+		glm::i32vec3 chunkItr{0};
+		glm::i32vec3 axisMask{0};
 
+		axisMask[axis] = 1;
 
-            glm::i32vec3 ChunkItr{0};
-            glm::i32vec3 AxisMask{0};
-            // glm::i32vec3 testTmp{0};
+		GreedyMask mask{axisLimit, axisLimit};
 
-            AxisMask[Axis] = 1;
+		// Check each slice of the chunk
+		for (chunkItr[axis] = -1; chunkItr[axis] < axisLimit;) {
+			// Compute Mask
+			mask.calculate(voxels, chunkItr, axis1, axis2, axisMask);
 
-            GreedyMask Mask{Axis1Limit, Axis2Limit};
+			chunkItr[axis]++;
 
-            // Check each slice of the chunk
-            // testTmp[Axis] = -1;
-            for (ChunkItr[Axis] = -1; ChunkItr[Axis] < MainAxisLimit;) {
-                // Compute Mask
-                Mask.calculate(voxels, ChunkItr, Axis1, Axis2, AxisMask);
+			// Generate Mesh From Mask
+			generateMesh(mask, axis1, axis2, axisMask, chunkItr);
+		}
+	}
+}
 
-                // testTmp[Axis]++;
-                ChunkItr[Axis]++;
+void vkx::VoxelChunk::generateMesh(GreedyMask& mask, std::int32_t axis1, std::int32_t axis2, const glm::i32vec3& axisMask, glm::i32vec3& chunkItr) {
+	for (int j = 0; j < axisLimit; ++j) {
+		for (int i = 0; i < axisLimit;) {
+			const auto& currentMask = mask.at(i, j);
+			if (currentMask.normal != 0) {
+				chunkItr[axis1] = i;
+				chunkItr[axis2] = j;
 
-                // Generate Mesh From Mask
-                generateMesh(Mask, Axis1, Axis2, AxisMask, ChunkItr);
-            }
-        }
-    }
+				const auto width = mask.calculateQuadWidth(currentMask, i, j);
+				const auto height = mask.calculateQuadHeight(currentMask, width, i, j);
 
-    static void printVec(const glm::vec3 &vec) {
-        std::cout << '(' << vec.x << ',' << vec.y << ',' << vec.z << ")\n";
-    }
+				glm::i32vec3 deltaAxis1{0};
+				glm::i32vec3 deltaAxis2{0};
+				deltaAxis1[axis1] = width;
+				deltaAxis2[axis2] = height;
 
-    static glm::vec3 test2(const glm::vec3 &position, const glm::vec3 &voxelPosition) {
-        const bool upperBound = position.x < voxelPosition.x - 1.0f &&
-                                position.y < voxelPosition.y - 1.0f &&
-                                position.z < voxelPosition.z - 1.0f;
-        const bool lowerBound = position.x < voxelPosition.x + 1.0f &&
-                                position.y < voxelPosition.y + 1.0f &&
-                                position.z < voxelPosition.z + 1.0f;
-        if (upperBound && lowerBound) {
-            return voxelPosition - 1.0f;
-        }
-        return position;
-    }
+				createQuad(
+				    currentMask, axisMask, width, height,
+				    chunkItr,
+				    chunkItr + deltaAxis1,
+				    chunkItr + deltaAxis2,
+				    chunkItr + deltaAxis1 + deltaAxis2);
 
-    glm::vec3 VoxelChunk::test(const glm::vec3 &position) {
-        // Due to how the mesh is rendered the world position is going to be subtracted from
-        const bool upperBound = position.x > worldPosition.x - static_cast<float>(voxels.getWidth()) &&
-                                position.y > worldPosition.y - static_cast<float>(voxels.getHeight()) &&
-                                position.z > worldPosition.z - static_cast<float>(voxels.getDepth());
-        // Continued here the position must be less than the world position due to the end of the mesh is far lower.
-        const bool lowerBound = position.x < worldPosition.x &&
-                                position.y < worldPosition.y &&
-                                position.z < worldPosition.z;
+				mask.clear(i, j, width, height);
 
-        if (upperBound && lowerBound) {
-            const auto voxelPosition = worldPosition - position;
-            const auto &voxel = voxels.at(voxelPosition);
-        }
+				i += width;
+			} else {
+				i++;
+			}
+		}
+	}
+}
 
-        return position;
-    }
+void vkx::VoxelChunk::createQuad(
+    VoxelMask mask,
+    const glm::ivec3& axisMask,
+    const int width,
+    const int height,
+    const glm::ivec3& V1,
+    const glm::ivec3& V2,
+    const glm::ivec3& V3,
+    const glm::ivec3& V4) {
+	auto normal = axisMask * mask.normal;
 
-    void
-    VoxelChunk::generateMesh(GreedyMask &Mask, std::int32_t Axis1, std::int32_t Axis2, glm::i32vec3 const &AxisMask,
-                             glm::i32vec3 &ChunkItr) {
+	auto v1 = vkx::Vertex{-V1, glm::vec2(0.0f, 0.0f), -normal};
+	auto v2 = vkx::Vertex{-V2, glm::vec2(width, 0.0f), -normal};
+	auto v3 = vkx::Vertex{-V3, glm::vec2(0.0f, height), -normal};
+	auto v4 = vkx::Vertex{-V4, glm::vec2(width, height), -normal};
 
-        for (int j = 0; j < Mask.getHeight(); ++j) {
-            for (int i = 0; i < Mask.getWidth();) {
-                auto const &CurrentMask = Mask.at(i, j);
-                if (CurrentMask.normal != 0) {
-                    ChunkItr[Axis1] = i;
-                    ChunkItr[Axis2] = j;
+	vertices.push_back(v1);
+	vertices.push_back(v2);
+	vertices.push_back(v3);
+	vertices.push_back(v4);
 
-                    auto Width = Mask.calculateQuadWidth(CurrentMask, i, j);
-                    auto Height = Mask.calculateQuadHeight(CurrentMask, Width, i, j);
+	indices.push_back(vertexCount);
+	indices.push_back(vertexCount + 2 + mask.normal);
+	indices.push_back(vertexCount + 2 - mask.normal);
+	indices.push_back(vertexCount + 3);
+	indices.push_back(vertexCount + 1 - mask.normal);
+	indices.push_back(vertexCount + 1 + mask.normal);
 
-                    glm::i32vec3 DeltaAxis1{0};
-                    glm::i32vec3 DeltaAxis2{0};
-                    DeltaAxis1[Axis1] = Width;
-                    DeltaAxis2[Axis2] = Height;
-
-                    CreateQuad(
-                            CurrentMask, AxisMask, Width, Height,
-                            ChunkItr,
-                            ChunkItr + DeltaAxis1,
-                            ChunkItr + DeltaAxis2,
-                            ChunkItr + DeltaAxis1 + DeltaAxis2);
-
-                    Mask.clear(i, j, Width, Height);
-
-                    i += Width;
-                } else {
-                    i++;
-                }
-            }
-        }
-    }
-
-    void VoxelChunk::CreateQuad(
-            VoxelMask mask,
-            const glm::ivec3 &axisMask,
-            const int width,
-            const int height,
-            const glm::ivec3 &V1,
-            const glm::ivec3 &V2,
-            const glm::ivec3 &V3,
-            const glm::ivec3 &V4) {
-        auto normal = axisMask * mask.normal;
-
-        auto v1 = vkx::Vertex{-V1, glm::vec2(0.0f, 0.0f), -normal};
-        auto v2 = vkx::Vertex{-V2, glm::vec2(width, 0.0f), -normal};
-        auto v3 = vkx::Vertex{-V3, glm::vec2(0.0f, height), -normal};
-        auto v4 = vkx::Vertex{-V4, glm::vec2(width, height), -normal};
-
-        vertices.push_back(v1);
-        vertices.push_back(v2);
-        vertices.push_back(v3);
-        vertices.push_back(v4);
-
-        indices.push_back(vertexCount);
-        indices.push_back(vertexCount + 2 + mask.normal);
-        indices.push_back(vertexCount + 2 - mask.normal);
-        indices.push_back(vertexCount + 3);
-        indices.push_back(vertexCount + 1 - mask.normal);
-        indices.push_back(vertexCount + 1 + mask.normal);
-
-        vertexCount += 4;
-    }
+	vertexCount += 4;
 }
