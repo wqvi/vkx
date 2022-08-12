8c528ab05776c9e5226ef7d2ad05fe4454bf83ef
voxelmesh <voxelmesh@proton.me>
2022-08-12 09:41:13 -0700

    Removed commands from device file

--- include/vkx/renderer/core/device.hpp
+++ include/vkx/renderer/core/device.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vkx/renderer/core/allocator.hpp>
+#include <vkx/renderer/core/commands.hpp>
 #include <vkx/renderer/core/pipeline.hpp>
 #include <vkx/renderer/core/swapchain.hpp>
 
@@ -47,44 +48,5 @@ private:
 	vk::PhysicalDevice physicalDevice{};
 	vk::UniqueDevice device{};
 	float maxSamplerAnisotropy = 0.0f;
-};
-
-struct DrawInfo {
-	vk::RenderPass renderPass;
-	vk::Framebuffer framebuffer;
-	vk::Extent2D extent;
-	vk::Pipeline graphicsPipeline;
-	vk::PipelineLayout graphicsPipelineLayout;
-	vk::DescriptorSet descriptorSet;
-	vk::Buffer vertexBuffer;
-	vk::Buffer indexBuffer;
-	std::uint32_t indexCount;
-};
-
-class CommandSubmitter {
-	using iter = std::vector<vk::CommandBuffer>::const_iterator;
-
-public:
-	explicit CommandSubmitter(vk::PhysicalDevice physicalDevice, vk::Device device, vk::SurfaceKHR surface);
-
-	void submitImmediately(const std::function<void(vk::CommandBuffer)>& command) const;
-
-	void transitionImageLayout(vk::Image image, vk::ImageLayout oldLayout, vk::ImageLayout newLayout) const;
-
-	void copyBufferToImage(vk::Buffer buffer, vk::Image image, std::uint32_t width, std::uint32_t height) const;
-
-	std::vector<vk::CommandBuffer> allocateDrawCommands(std::uint32_t amount) const;
-
-	void recordDrawCommands(iter begin, iter end, const DrawInfo& drawInfo) const;
-
-	void submitDrawCommands(iter begin, iter end, const SyncObjects& syncObjects) const;
-
-	vk::Result presentToSwapchain(vk::SwapchainKHR swapchain, std::uint32_t imageIndex, const SyncObjects& syncObjects) const;
-
-private:
-	vk::Device device{};
-	vk::UniqueCommandPool commandPool{};
-	vk::Queue graphicsQueue{};
-	vk::Queue presentQueue{};
 };
 } // namespace vkx
