5c558ad5491d06c84f46d14f52fbe3d271912f00
voxelmesh <voxelmesh@proton.me>
2022-08-12 09:47:31 -0700

    Tidied model file

--- include/vkx/renderer/model.hpp
+++ include/vkx/renderer/model.hpp
@@ -1,17 +1,13 @@
 #pragma once
 
-#include "vkx/renderer/core/device.hpp"
-#include "vkx/renderer/core/vertex.hpp"
-#include <array>
 #include <vkx/renderer/image.hpp>
-#include <vkx/renderer/uniform_buffer.hpp>
 
 namespace vkx {
 class Mesh {
 public:
 	Mesh() = default;
 
-	explicit Mesh(const std::vector<Vertex>& vertices, const std::vector<std::uint32_t>& indices, const Device& device, const std::shared_ptr<Allocator>& allocator);
+	explicit Mesh(const std::vector<Vertex>& vertices, const std::vector<std::uint32_t>& indices, const std::shared_ptr<Allocator>& allocator);
 
 	template <std::size_t T, std::size_t K>
 	explicit Mesh(const std::array<Vertex, T>& vertices, const std::array<std::uint32_t, K>& indices, const std::shared_ptr<Allocator>& allocator)
@@ -28,24 +24,11 @@ public:
 
 	Texture(const std::string& file, const Device& device, const std::shared_ptr<vkx::Allocator>& allocator, const std::shared_ptr<vkx::CommandSubmitter>& commandSubmitter);
 
-	[[nodiscard]] vk::WriteDescriptorSet createWriteDescriptorSet(const vk::DescriptorSet& descriptorSet, std::uint32_t dstBinding) const;
+	[[nodiscard]] vk::WriteDescriptorSet createWriteDescriptorSet(vk::DescriptorSet descriptorSet, std::uint32_t dstBinding) const;
 
 private:
 	Image image;
 	vk::UniqueImageView view;
 	vk::UniqueSampler sampler;
-};
-
-struct Model {
-	Model() = default;
-
-	explicit Model(Mesh&& mesh, Texture&& texture, const Material& material);
-
-	[[nodiscard]] glm::mat4 getModelMatrix() const noexcept;
-
-	Mesh mesh;
-	Texture texture;
-	Material material = {};
-	glm::vec3 position = glm::vec3(0);
 };
 } // namespace vkx
--- src/renderer/model.cpp
+++ src/renderer/model.cpp
@@ -1,45 +1,30 @@
-#include "vkx/renderer/core/device.hpp"
+#include <vkx/renderer/core/device.hpp>
 #include <vkx/renderer/model.hpp>
-#include <vulkan/vulkan_enums.hpp>
 
-namespace vkx {
-Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<std::uint32_t>& indices, const Device& device, const std::shared_ptr<Allocator>& allocator)
+vkx::Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<std::uint32_t>& indices, const std::shared_ptr<Allocator>& allocator)
     : vertex(allocator->allocateBuffer(vertices, vk::BufferUsageFlagBits::eVertexBuffer)), index(allocator->allocateBuffer(indices, vk::BufferUsageFlagBits::eIndexBuffer)), indexCount(indices.size()) {}
 
-Texture::Texture(const std::string& file, const Device& device, const std::shared_ptr<vkx::Allocator>& allocator, const std::shared_ptr<vkx::CommandSubmitter>& commandSubmitter)
+vkx::Texture::Texture(const std::string& file, const Device& device, const std::shared_ptr<vkx::Allocator>& allocator, const std::shared_ptr<vkx::CommandSubmitter>& commandSubmitter)
     : image(file, allocator, commandSubmitter),
       view(device.createTextureImageViewUnique(image.resource->object)),
       sampler(device.createTextureSamplerUnique()) {
 }
 
-vk::WriteDescriptorSet
-Texture::createWriteDescriptorSet(const vk::DescriptorSet& descriptorSet, std::uint32_t dstBinding) const {
+vk::WriteDescriptorSet vkx::Texture::createWriteDescriptorSet(vk::DescriptorSet descriptorSet, std::uint32_t dstBinding) const {
 	static vk::DescriptorImageInfo info{};
 
-	info = vk::DescriptorImageInfo{
-	    *sampler,				    // sampler
-	    *view,				    // imageView
-	    vk::ImageLayout::eShaderReadOnlyOptimal // imageLayout
-	};
+	info = vk::DescriptorImageInfo(*sampler, *view, vk::ImageLayout::eShaderReadOnlyOptimal);
 
 	static vk::WriteDescriptorSet set{};
 
-	set = vk::WriteDescriptorSet{
-	    descriptorSet,			       // dstSet
-	    dstBinding,				       // dstBinding
-	    0,					       // dstArrayElement
-	    1,					       // descriptorCount
-	    vk::DescriptorType::eCombinedImageSampler, // descriptorType
-	    &info,				       // pImageInfo
-	    nullptr				       // pBufferInfo
-	};
+	set = vk::WriteDescriptorSet(
+	    descriptorSet,
+	    dstBinding,
+	    0,
+	    1,
+	    vk::DescriptorType::eCombinedImageSampler,
+	    &info,
+	    nullptr);
+
 	return set;
-}
-} // namespace vkx
-
-vkx::Model::Model(Mesh&& mesh, Texture&& texture, const Material& material)
-    : mesh(std::move(mesh)), texture(std::move(texture)), material(material) {}
-
-glm::mat4 vkx::Model::getModelMatrix() const noexcept {
-	return glm::translate(glm::mat4(1), position);
 }
