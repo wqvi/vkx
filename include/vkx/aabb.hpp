fdc948f7b2f2dbe931bdd663445af2175887dd35
voxelmesh <voxelmesh@proton.me>
2022-08-26 08:03:11 -0700

    Changed to struct

--- include/vkx/aabb.hpp
+++ include/vkx/aabb.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 namespace vkx {
-class AABB {
-public:
+struct AABB {
+	glm::vec3 min = glm::vec3{0, 0, 0};
+	glm::vec3 max = glm::vec3{-1, -1, -1};
+
 	AABB() = default;
 
 	AABB(const glm::vec3& min, const glm::vec3& max);
@@ -14,9 +16,5 @@ public:
 	bool intersects(const glm::ivec3& point) const;
 
 	bool intersects(const AABB& other) const;
-
-private:
-	glm::vec3 min = glm::vec3(0, 0, 0);
-	glm::vec3 max = glm::vec3(-1, -1, -1);
 };
 } // namespace vkx
