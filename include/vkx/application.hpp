5d5f6e59dc3512c524a21e324cc10dc56f09ea43
voxelmesh <voxelmesh@proton.me>
2022-08-26 11:14:25 -0700

    Working on application

--- include/vkx/application.hpp
+++ include/vkx/application.hpp
@@ -1,22 +1,55 @@
 #pragma once
 
+#include <vkx/renderer/core/bootstrap.hpp>
+
 namespace vkx {
+class Renderer {
+private:
+    const RendererBootstrap bootstrap;
+public:
+    explicit Renderer(const SDL_Window* window);
+};
+
+class SDLWindow {
+private:
+    struct SDLDeleter {
+        void operator()(SDL_Window* ptr) noexcept;
+    };
+
+    std::unique_ptr<SDL_Window, SDLDeleter> window;
+
+public:
+    explicit SDLWindow(const char* title, int width, int height);
+
+    explicit operator SDL_Window*() const noexcept;
+};
+
 class Application {
+private:
+    int state = 1;
+
+    SDLWindow window;
+
+    // Renderer renderer;
+
 public:
 	Application();
 
-	Application(const Application&) = default;
+	Application(const Application&) = delete;
 
 	Application(Application&&) noexcept = default;
 
     virtual ~Application();
 
-	Application& operator=(const Application& other) = default;
+	Application& operator=(const Application& other) = delete;
 
 	Application& operator=(Application&&) noexcept = default;
 
 	virtual void init() = 0;
 
 	virtual void destroy() = 0;
+
+private:
+    static int SDLInit();
 };
 } // namespace vkx
--- src/application.cpp
+++ src/application.cpp
@@ -1,11 +1,38 @@
 #include <vkx/application.hpp>
 
-#include <iostream>
+void vkx::SDLWindow::SDLDeleter::operator()(SDL_Window* ptr) noexcept {
+	if (ptr) {
+		SDL_DestroyWindow(ptr);
+	}
+}
 
-vkx::Application::Application() {
-    std::cout << "Initializing Application\n";   
+vkx::SDLWindow::SDLWindow(const char* title, int width, int height)
+    : window(std::unique_ptr<SDL_Window, vkx::SDLWindow::SDLDeleter>(SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_VULKAN))) {
+	if (window == nullptr) {
+		throw std::runtime_error(SDL_GetError());
+	}
+}
+
+vkx::Application::Application()
+    : state(SDLInit()), window("vkx", 640, 360) {
+        //, renderer(static_cast<SDL_Window*>(window)) {
+	if (SDL_SetRelativeMouseMode(SDL_TRUE)) {
+		throw std::runtime_error(SDL_GetError());
+	}
 }
 
 vkx::Application::~Application() {
-    std::cout << "Destroying Application\n";
+	if (state == 0) {
+		SDL_Quit();
+	}
+}
+
+int vkx::Application::SDLInit() {
+	int state = SDL_Init(SDL_INIT_EVERYTHING);
+
+	if (state != 0) {
+		throw std::runtime_error(SDL_GetError());
+	}
+
+	return state;
 }
