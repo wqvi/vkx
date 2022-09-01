ab72e4705e81792fac1978b08ea0d250785f2355
voxelmesh <voxelmesh@proton.me>
2022-09-01 08:32:14 -0700

    Added cube model to PCH

--- include/vkx/pch.hpp
+++ include/vkx/pch.hpp
@@ -1,35 +1,77 @@
 #pragma once
 
 #define VULKAN_HPP_NO_SPACESHIP_OPERATOR
-#include <vulkan/vulkan.hpp>
-#include <vk_mem_alloc.h>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_vulkan.h>
 #include <SDL2/SDL_log.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vk_mem_alloc.h>
+#include <vulkan/vulkan.hpp>
 #define GLM_FORCE_LEFT_HANDED
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <chrono>
+#include <forward_list>
+#include <fstream>
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <string>
-#include <array>
-#include <vector>
-#include <set>
-#include <fstream>
-#include <algorithm>
-#include <stdexcept>
+#include <list>
 #include <memory>
 #include <optional>
+#include <set>
+#include <stdexcept>
+#include <string>
 #include <unordered_map>
-#include <chrono>
-#include <bitset>
-#include <list>
-#include <forward_list>
 #include <variant>
+#include <vector>
 
 #ifdef NDEBUG
 #define RELEASE
 #else
 #define DEBUG
 #endif
+
+namespace vkx {
+static constexpr std::array<glm::vec3, 24> CUBE_VERTICES = {
+    glm::vec3{0.0f, 0.0f, 0.0f},
+    {0.0f, 1.0f, 0.0f},
+    {1.0f, 1.0f, 0.0f},
+    {1.0f, 0.0f, 0.0f},
+
+    {1.0f, 0.0f, 0.0f},
+    {1.0f, 1.0f, 0.0f},
+    {1.0f, 1.0f, 1.0f},
+    {1.0f, 0.0f, 1.0f},
+
+    {1.0f, 0.0f, 1.0f},
+    {1.0f, 1.0f, 1.0f},
+    {0.0f, 1.0f, 1.0f},
+    {0.0f, 0.0f, 1.0f},
+
+    {0.0f, 0.0f, 1.0f},
+    {0.0f, 1.0f, 1.0f},
+    {0.0f, 1.0f, 0.0f},
+    {0.0f, 0.0f, 0.0f},
+
+    {0.0f, 0.0f, 0.0f},
+    {1.0f, 0.0f, 0.0f},
+    {1.0f, 0.0f, 1.0f},
+    {0.0f, 0.0f, 1.0f},
+
+    {0.0f, 1.0f, 0.0f},
+    {0.0f, 1.0f, 1.0f},
+    {1.0f, 1.0f, 1.0f},
+    {1.0f, 1.0f, 0.0f},
+};
+
+static constexpr std::array<std::uint32_t, 36> CUBE_INDICES = {
+    0, 1, 2, 2, 3, 0,
+    4, 5, 6, 6, 7, 4,
+    8, 9, 10, 10, 11, 8,
+    12, 13, 14, 14, 15, 12,
+    16, 17, 18, 18, 19, 16,
+    20, 21, 22, 22, 23, 20};
+}
