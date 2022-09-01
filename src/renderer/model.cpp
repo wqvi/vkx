562cae6e6b5aefde0c19aa7c00347e264b593e66
voxelmesh <voxelmesh@proton.me>
2022-09-01 09:05:46 -0700

    Removed shared ptr

--- src/renderer/model.cpp
+++ src/renderer/model.cpp
@@ -1,10 +1,10 @@
 #include <vkx/renderer/core/device.hpp>
 #include <vkx/renderer/model.hpp>
 
-vkx::Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<std::uint32_t>& indices, const std::shared_ptr<Allocator>& allocator)
-    : vertex(allocator->allocateBuffer(vertices, vk::BufferUsageFlagBits::eVertexBuffer)), index(allocator->allocateBuffer(indices, vk::BufferUsageFlagBits::eIndexBuffer)), indexCount(indices.size()) {}
+vkx::Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<std::uint32_t>& indices, const vkx::Allocator& allocator)
+    : vertex(allocator.allocateBuffer(vertices, vk::BufferUsageFlagBits::eVertexBuffer)), index(allocator.allocateBuffer(indices, vk::BufferUsageFlagBits::eIndexBuffer)), indexCount(indices.size()) {}
 
-vkx::Texture::Texture(const std::string& file, const Device& device, const std::shared_ptr<vkx::Allocator>& allocator, const std::shared_ptr<vkx::CommandSubmitter>& commandSubmitter)
+vkx::Texture::Texture(const std::string& file, const Device& device, const vkx::Allocator& allocator, const std::shared_ptr<vkx::CommandSubmitter>& commandSubmitter)
     : image(file, allocator, commandSubmitter),
       view(device.createTextureImageViewUnique(image.resource->object)),
       sampler(device.createTextureSamplerUnique()) {
