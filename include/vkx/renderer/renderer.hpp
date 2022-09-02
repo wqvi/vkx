380a1b32dd5295bb4431e7737ab7e68af8b17854
voxelmesh <voxelmesh@proton.me>
2022-09-02 07:51:04 -0700

    Added window based constructor

--- include/vkx/renderer/renderer.hpp
+++ include/vkx/renderer/renderer.hpp
@@ -1,6 +1,17 @@
 #pragma once
 
+#include <vkx/renderer/core/bootstrap.hpp>
+#include <vkx/renderer/core/device.hpp>
+#include <vkx/application.hpp>
+
 namespace vkx {
 class Renderer {
+private:
+    vkx::RendererBootstrap bootstrap{};
+
+public:
+    Renderer() = default;
+
+    explicit Renderer(const SDLWindow& window);
 };
 } // namespace vkx
--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,3 +1,5 @@
+#include "vkx/renderer/core/bootstrap.hpp"
 #include <vkx/renderer/renderer.hpp>
 
-vkx::Renderer::Renderer(const SDLWindow& window) {}
+vkx::Renderer::Renderer(const SDLWindow& window) 
+    : bootstrap(static_cast<SDL_Window*>(window)) {}
