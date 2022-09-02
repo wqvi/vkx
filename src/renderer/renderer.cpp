c0680036a7353a6b23a7bad54c60562008669f8a
voxelmesh <voxelmesh@proton.me>
2022-09-02 07:54:09 -0700

    Initialized new member variables

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -1,5 +1,10 @@
+#include "vkx/renderer/core/allocator.hpp"
 #include "vkx/renderer/core/bootstrap.hpp"
+#include "vkx/renderer/core/commands.hpp"
 #include <vkx/renderer/renderer.hpp>
 
 vkx::Renderer::Renderer(const SDLWindow& window) 
-    : bootstrap(static_cast<SDL_Window*>(window)) {}
+    : bootstrap(static_cast<SDL_Window*>(window)),
+    device(bootstrap.createDevice()),
+    allocator(device.createAllocator()),
+    commandSubmitter(device.createCommandSubmitter()) {}
