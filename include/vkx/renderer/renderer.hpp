cc37c58c568c659961bf0a6d3996f724bff3d9f4
voxelmesh <voxelmesh@proton.me>
2022-10-10 13:15:57 -0700

    House keeping

--- include/vkx/renderer/renderer.hpp
+++ include/vkx/renderer/renderer.hpp
@@ -7,73 +7,9 @@
 #include <vulkan/vulkan_handles.hpp>
 
 namespace vkx {
-struct DrawInfoTest {
-	const vk::CommandBufferLevel level = vk::CommandBufferLevel::eSecondary;
-	const GraphicsPipeline* graphicsPipeline = nullptr;
-	const std::vector<const vkx::Mesh*> meshes{};
-};
+[[nodiscard]] vk::UniqueInstance createInstance(SDL_Window* const window);
 
-class Renderer {
-private:
-	RendererBootstrap bootstrap{};
-	Device device{};
+[[nodiscard]] vk::UniqueSurfaceKHR createSurface(SDL_Window* const window, vk::Instance instance);
 
-public:
-	Allocator allocator{};
-	CommandSubmitter commandSubmitter{};
-
-private:
-	Swapchain swapchain{};
-	vk::UniqueRenderPass clearRenderPass{};
-	vk::UniqueRenderPass loadRenderPass{};
-
-	std::vector<std::shared_ptr<vkx::GraphicsPipeline>> pipelines{};
-
-public:
-	std::vector<vkx::SyncObjects> syncObjects{};
-
-	std::uint32_t currentFrame = 0;
-
-	std::uint32_t imageIndex = 0;
-
-	bool framebufferResized = false;
-
-	std::vector<vk::CommandBuffer> primaryCommandsBuffers{};
-	std::size_t primaryDrawCommandsAmount = 0;
-	std::vector<vk::CommandBuffer> secondaryCommandBuffers{};
-	std::size_t secondaryDrawCommandsAmount = 0;
-
-public:
-	Renderer() = default;
-
-	explicit Renderer(const SDLWindow& window);
-
-	std::shared_ptr<vkx::GraphicsPipeline> attachPipeline(const GraphicsPipelineInformation& pipelineInformation);
-
-	void resized(const SDLWindow& window);
-
-	template <class T, class K>
-	auto createMesh(const T& vertices, const K& indices) const {
-		return vkx::Mesh{vertices, indices, allocator};
-	}
-
-	vkx::Texture createTexture(const std::string& file) const;
-
-	void createDrawCommands(const std::vector<DrawInfoTest>& drawInfos);
-
-	void lazySync(const vkx::SDLWindow& window);
-
-	void uploadDrawCommands(const std::vector<DrawInfoTest>& drawInfos, const SDLWindow& window);
-
-	void lazyUpdate();
-};
-
-[[nodiscard]]
-vk::UniqueInstance createInstance(SDL_Window* const window);
-
-[[nodiscard]]
-vk::UniqueSurfaceKHR createSurface(SDL_Window* const window, vk::Instance instance);
-
-[[nodiscard]]
-vk::PhysicalDevice getBestPhysicalDevice(vk::Instance instance, vk::SurfaceKHR surface);
+[[nodiscard]] vk::PhysicalDevice getBestPhysicalDevice(vk::Instance instance, vk::SurfaceKHR surface);
 } // namespace vkx
--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -2,121 +2,7 @@
 #include <vkx/renderer/renderer.hpp>
 #include <vulkan/vulkan_handles.hpp>
 
-vkx::Renderer::Renderer(const SDLWindow& window)
-    : bootstrap(static_cast<SDL_Window*>(window)),
-      device(bootstrap.createDevice()),
-      allocator(device.createAllocator()),
-      commandSubmitter(device.createCommandSubmitter()) {
-	const vkx::SwapchainInfo swapchainInfo{device};
-	const auto surfaceFormat = swapchainInfo.chooseSurfaceFormat().format;
-
-	clearRenderPass = device.createRenderPass(surfaceFormat, vk::ImageLayout::eUndefined, vk::ImageLayout::eColorAttachmentOptimal, vk::AttachmentLoadOp::eClear);
-	loadRenderPass = device.createRenderPass(surfaceFormat, vk::ImageLayout::eColorAttachmentOptimal, vk::ImageLayout::ePresentSrcKHR, vk::AttachmentLoadOp::eLoad);
-
-	swapchain = device.createSwapchain(static_cast<SDL_Window*>(window), *clearRenderPass, allocator);
-
-	syncObjects = device.createSyncObjects();
-}
-
-std::shared_ptr<vkx::GraphicsPipeline> vkx::Renderer::attachPipeline(const vkx::GraphicsPipelineInformation& pipelineInformation) {
-	pipelines.push_back(device.createGraphicsPipeline(*clearRenderPass, allocator, pipelineInformation));
-	return pipelines.back();
-}
-
-void vkx::Renderer::resized(const SDLWindow& window) {
-	auto [newWidth, newHeight] = window.getSize();
-	while (newWidth == 0 || newHeight == 0) {
-		std::tie(newWidth, newHeight) = window.getSize();
-		SDL_WaitEvent(nullptr);
-	}
-
-	device->waitIdle();
-
-	swapchain = device.createSwapchain(static_cast<SDL_Window*>(window), *clearRenderPass, allocator);
-}
-
-vkx::Texture vkx::Renderer::createTexture(const std::string& file) const {
-	return vkx::Texture{file, device, allocator, commandSubmitter};
-}
-
-void vkx::Renderer::createDrawCommands(const std::vector<DrawInfoTest>& drawInfos) {
-	if (drawInfos.empty()) {
-		throw std::invalid_argument("Draw infos can't be empty.");
-	}
-
-	for (const auto& drawInfo : drawInfos) {
-		if (drawInfo.level == vk::CommandBufferLevel::eSecondary) {
-			secondaryDrawCommandsAmount += drawInfo.meshes.size();
-		}
-	}
-
-	primaryCommandsBuffers = commandSubmitter.allocateDrawCommands(drawInfos.size());
-
-	if (secondaryDrawCommandsAmount != 0) {
-		secondaryCommandBuffers = commandSubmitter.allocateSecondaryDrawCommands(secondaryDrawCommandsAmount);
-	}
-
-	primaryDrawCommandsAmount = drawInfos.size();
-}
-
-void vkx::Renderer::lazySync(const vkx::SDLWindow& window) {
-	const auto& syncObject = syncObjects[currentFrame];
-	syncObject.waitForFence();
-	vk::Result result = vk::Result::eSuccess;
-	std::tie(result, imageIndex) = swapchain.acquireNextImage(device, syncObject);
-
-	if (result == vk::Result::eErrorOutOfDateKHR) {
-		resized(window);
-	} else if (result != vk::Result::eSuccess && result != vk::Result::eSuboptimalKHR) {
-		throw std::runtime_error("Failed to acquire next image.");
-	}
-
-	syncObject.resetFence();
-}
-
-void vkx::Renderer::uploadDrawCommands(const std::vector<DrawInfoTest>& drawInfos, const SDLWindow& window) {
-	const auto& syncObject = syncObjects[currentFrame];
-
-	const vk::CommandBuffer* primaryBegin = &primaryCommandsBuffers[currentFrame * primaryDrawCommandsAmount];
-
-	const vk::CommandBuffer* secondaryBegin = &secondaryCommandBuffers[currentFrame * secondaryDrawCommandsAmount];
-
-	std::uintptr_t offset = 0;
-
-	for (std::uint32_t i = 0; i < drawInfos.size(); i++) {
-		const auto& drawInfo = drawInfos[i];
-
-		const vk::CommandBuffer* primary = primaryBegin + static_cast<std::uintptr_t>(i);
-
-		const vk::CommandBuffer* secondary = secondaryBegin + offset;
-
-		if (drawInfo.level == vk::CommandBufferLevel::ePrimary) {
-			commandSubmitter.recordPrimaryDrawCommands(nullptr, 0, {});
-		} else {
-			commandSubmitter.recordSecondaryDrawCommands(nullptr, 0, nullptr, 0, {});
-		}
-
-		offset += static_cast<std::uintptr_t>(drawInfo.meshes.size());
-	}
-
-	commandSubmitter.submitDrawCommands(primaryBegin, primaryDrawCommandsAmount, syncObject);
-
-	const auto result = commandSubmitter.presentToSwapchain(swapchain, imageIndex, syncObject);
-
-	if (result == vk::Result::eErrorOutOfDateKHR || result == vk::Result::eSuboptimalKHR || framebufferResized) {
-		framebufferResized = false;
-
-		resized(window);
-	} else if (result != vk::Result::eSuccess) {
-		throw std::runtime_error("Failed to present.");
-	}
-}
-
-void vkx::Renderer::lazyUpdate() {
-	currentFrame = (currentFrame + 1) % vkx::MAX_FRAMES_IN_FLIGHT;
-}
-
-vk::UniqueInstance vkx::createInstance(SDL_Window *const window) {
+vk::UniqueInstance vkx::createInstance(SDL_Window* const window) {
 	constexpr vk::ApplicationInfo applicationInfo{"VKX", VK_MAKE_VERSION(0, 0, 1), "VKX", VK_MAKE_VERSION(0, 0, 1), VK_API_VERSION_1_0};
 
 	std::uint32_t count = 0;
@@ -155,7 +41,7 @@ vk::UniqueInstance vkx::createInstance(SDL_Window *const window) {
 #endif
 }
 
-vk::UniqueSurfaceKHR vkx::createSurface(SDL_Window *const window, vk::Instance instance) {
+vk::UniqueSurfaceKHR vkx::createSurface(SDL_Window* const window, vk::Instance instance) {
 	VkSurfaceKHR cSurface = nullptr;
 	if (SDL_Vulkan_CreateSurface(window, instance, &cSurface) != SDL_TRUE) {
 		throw std::runtime_error("Failed to create vulkan surface.");
