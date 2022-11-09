4836bc804c6fc5f7cdc74689420db4b17481e4fd
voxelmesh <voxelmesh@proton.me>
2022-11-08 18:04:37 -0800

    New allocator and constexpr quality of life functions

--- include/vkx/renderer/renderer.hpp
+++ include/vkx/renderer/renderer.hpp
@@ -5,6 +5,32 @@
 #include "core/sync_objects.hpp"
 
 namespace vkx {
+template <class ArrayType, class Function, class Predicate, class... Parameters>
+constexpr auto get(const char* errorMessage, Function function, Predicate predicate, Parameters... param) {
+	std::uint32_t count = 0;
+	auto result = function(param..., &count, nullptr);
+	if (predicate(result)) {
+		throw std::runtime_error(errorMessage);
+	}
+
+	std::vector<ArrayType> items{count};
+	result = function(param..., &count, items.data());
+	if (predicate(result)) {
+		throw std::runtime_error(errorMessage);
+	}
+
+	return items;
+}
+
+template <class ObjectType, class Function, class Predicate, class... Parameters>
+constexpr auto create(Function function, Predicate predicate, Parameters... param) {
+	ObjectType object{};
+	auto result = function(param..., &object);
+	predicate(result);
+
+	return object;
+}
+
 [[nodiscard]] VkInstance createInstance(SDL_Window* const window);
 
 [[nodiscard]] VkSurfaceKHR createSurface(SDL_Window* const window, VkInstance instance);
@@ -15,14 +41,14 @@ namespace vkx {
 
 [[nodiscard]] VkFormat findSupportedFormat(VkPhysicalDevice physicalDevice, VkImageTiling tiling, VkFormatFeatureFlags features, const std::vector<VkFormat>& candidates);
 
-[[nodiscard]] inline auto findDepthFormat(vk::PhysicalDevice physicalDevice) {
+[[nodiscard]] inline auto findDepthFormat(VkPhysicalDevice physicalDevice) {
 	return findSupportedFormat(physicalDevice, VK_IMAGE_TILING_OPTIMAL, VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT, {VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT});
 }
 
-[[nodiscard]] vk::UniqueImageView createImageViewUnique(vk::Device device, vk::Image image, vk::Format format, vk::ImageAspectFlags aspectFlags);
+[[nodiscard]] VkImageView createImageView(VkDevice device, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
 
-[[nodiscard]] inline vk::UniqueImageView createTextureImageViewUnique(vk::Device device, vk::Image image) {
-	return createImageViewUnique(device, image, vk::Format::eR8G8B8A8Srgb, vk::ImageAspectFlagBits::eColor);
+[[nodiscard]] inline auto createTextureImageView(VkDevice device, VkImage image) {
+	return createImageView(device, image, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT);
 }
 
 [[nodiscard]] vk::UniqueSampler createTextureSamplerUnique(vk::Device device, float samplerAnisotropy);
@@ -31,7 +57,9 @@ namespace vkx {
 
 [[nodiscard]] std::vector<vkx::SyncObjects> createSyncObjects(vk::Device device);
 
-[[nodiscard]] vk::UniqueSwapchainKHR createSwapchainUnique(vk::Device device, vk::SurfaceKHR surface, SDL_Window* window, const vkx::SwapchainInfo& info, const vkx::QueueConfig& config);
+[[nodiscard]] VkSwapchainKHR createSwapchain(VkDevice device, VkSurfaceKHR surface, SDL_Window* window, const vkx::SwapchainInfo& info, const vkx::QueueConfig& config);
 
 [[nodiscard]] VmaAllocator createAllocator(VkPhysicalDevice physicalDevice, VkDevice device, VkInstance instance);
+
+[[nodiscard]] VmaAllocation allocateImage(VmaAllocationInfo* allocationInfo, VkImage* image, VmaAllocator allocator, std::uint32_t width, std::uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags imageUsage, VmaAllocationCreateFlags flags = VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT, VmaMemoryUsage memoryUsage = VMA_MEMORY_USAGE_AUTO);
 } // namespace vkx
--- src/renderer/core/swapchain.cpp
+++ src/renderer/core/swapchain.cpp
@@ -4,53 +4,70 @@
 #include <vkx/renderer/core/allocator.hpp>
 #include <vkx/renderer/renderer.hpp>
 
-vkx::Swapchain::Swapchain(vk::Device device, vk::PhysicalDevice physicalDevice, vk::RenderPass renderPass, vk::SurfaceKHR surface, SDL_Window* window, const Allocator& allocator) {
-	if (!static_cast<bool>(surface)) {
-		throw std::invalid_argument("Surface must be a valid handle to create a swapchain.");
-	}
-
+vkx::Swapchain::Swapchain(VkDevice device, VkPhysicalDevice physicalDevice, VkRenderPass renderPass, VkSurfaceKHR surface, SDL_Window* window, const Allocator& allocator) 
+	: device(device), allocator(allocator.getAllocator()) {
 	const SwapchainInfo info{physicalDevice, surface};
 	const QueueConfig config{physicalDevice, surface};
 
-	swapchain = vkx::createSwapchainUnique(device, surface, window, info, config);
+	swapchain = vkx::createSwapchain(device, surface, window, info, config);
 
-	images = device.getSwapchainImagesKHR(*swapchain);
-
+	images = vkx::get<VkImage>("Failed to retrieve swapchain images", vkGetSwapchainImagesKHR, [](auto a) { return a != VK_SUCCESS; }, device, swapchain);
+	
 	int width;
 	int height;
 	SDL_Vulkan_GetDrawableSize(window, &width, &height);
 
 	const auto imageFormat = info.chooseSurfaceFormat().format;
-	imageExtent = info.chooseExtent(width, height);
+	imageExtent = static_cast<VkExtent2D>(info.chooseExtent(width, height));
 
 	for (const auto image : images) {
-		imageViews.push_back(vkx::createImageViewUnique(static_cast<vk::Device>(device), image, imageFormat, vk::ImageAspectFlagBits::eColor));
+		imageViews.push_back(vkx::createImageView(device, image, static_cast<VkFormat>(imageFormat), VK_IMAGE_ASPECT_COLOR_BIT));
 	}
 
 	const auto depthFormat = vkx::findDepthFormat(physicalDevice);
-	depthResource = allocator.allocateImage(imageExtent.width, imageExtent.height, static_cast<vk::Format>(depthFormat), vk::ImageTiling::eOptimal, vk::ImageUsageFlagBits::eDepthStencilAttachment);
-	depthImageView = vkx::createImageViewUnique(static_cast<vk::Device>(device), depthResource->object, static_cast<vk::Format>(depthFormat), vk::ImageAspectFlagBits::eDepth);
+	depthAllocation = vkx::allocateImage(nullptr, &depthImage, allocator.getAllocator(), imageExtent.width, imageExtent.height, depthFormat, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT);
+	depthImageView = vkx::createImageView(device, depthImage, depthFormat, VK_IMAGE_ASPECT_DEPTH_BIT);
 
 	framebuffers.resize(imageViews.size());
 
 	for (std::size_t i = 0; i < imageViews.size(); i++) {
-		const std::array framebufferAttachments = {*imageViews[i], *depthImageView};
+		const std::array framebufferAttachments = {imageViews[i], depthImageView};
 
-		const vk::FramebufferCreateInfo framebufferInfo{
-		    {},
-		    renderPass,
-		    framebufferAttachments,
-		    imageExtent.width,
-		    imageExtent.height,
-		    1};
+		const VkFramebufferCreateInfo framebufferInfo{
+			VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO,
+			nullptr,
+			0,
+			renderPass,
+			static_cast<std::uint32_t>(framebufferAttachments.size()),
+			framebufferAttachments.data(),
+			imageExtent.width,
+			imageExtent.height,
+			1};
 
-		framebuffers[i] = device.createFramebufferUnique(framebufferInfo);
+		if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &framebuffers[i]) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create swapchain framebuffer");
+		}
 	}
 }
 
 vk::ResultValue<std::uint32_t> vkx::Swapchain::acquireNextImage(vk::Device device, const vkx::SyncObjects& syncObjects) const {
 	std::uint32_t imageIndex = 0;
-	const auto result = device.acquireNextImageKHR(*swapchain, UINT64_MAX, *syncObjects.imageAvailableSemaphore, {}, &imageIndex);
+	const auto result = vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, *syncObjects.imageAvailableSemaphore, {}, &imageIndex);
 
-	return {result, imageIndex};
+	return {static_cast<vk::Result>(result), imageIndex};
+}
+
+void vkx::Swapchain::destroy() {
+	vkDestroyImageView(device, depthImageView, nullptr);
+	vmaDestroyImage(allocator, depthImage, depthAllocation);
+
+	for (auto imageView : imageViews) {
+		vkDestroyImageView(device, imageView, nullptr);
+	}
+
+	for (auto framebuffer : framebuffers) {
+		vkDestroyFramebuffer(device, framebuffer, nullptr);
+	}
+
+	vkDestroySwapchainKHR(device, swapchain, nullptr);
 }
