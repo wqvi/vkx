1b6b5ac72090baf853d3025cc9bab63f00bec7be
voxelmesh <voxelmesh@proton.me>
2022-11-11 10:46:07 -0800

    Uses new command submitter functions

--- src/renderer/image.cpp
+++ src/renderer/image.cpp
@@ -38,31 +38,19 @@ vkx::Image::Image(const char* file, VmaAllocator allocator, const vkx::CommandSu
 
 	const auto imageSize = static_cast<VkDeviceSize>(texWidth * texHeight * static_cast<int>(STBI_rgb_alpha));
 
-	// const auto stagingResource = allocator.allocateBuffer(imageSize, vk::BufferUsageFlagBits::eTransferSrc, VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT, VMA_MEMORY_USAGE_AUTO_PREFER_HOST);
-
 	VmaAllocationInfo allocationInfo{};
 	VkBuffer stagingBuffer = nullptr;
 	const auto stagingAllocation = vkx::allocateBuffer(&allocationInfo, &stagingBuffer, allocator, imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_ALLOCATION_CREATE_HOST_ACCESS_SEQUENTIAL_WRITE_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT, VMA_MEMORY_USAGE_AUTO_PREFER_HOST);
 
-	// std::memcpy(stagingResource->allocationInfo.pMappedData, pixels, stagingResource->allocationInfo.size);
-
 	std::memcpy(allocationInfo.pMappedData, pixels, allocationInfo.size);
-
-	// resource = allocator.allocateImage(texWidth, texHeight, vk::Format::eR8G8B8A8Srgb, vk::ImageTiling::eOptimal, vk::ImageUsageFlagBits::eTransferDst | vk::ImageUsageFlagBits::eSampled, 0, VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE);
 
 	resourceAllocation = vkx::allocateImage(nullptr, &resourceImage, allocator, texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, 0, VMA_MEMORY_USAGE_AUTO_PREFER_DEVICE);
 
-	// commandSubmitter.transitionImageLayout(resource->object, vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal);
-
-	commandSubmitter.transitionImageLayout(resourceImage, vk::ImageLayout::eUndefined, vk::ImageLayout::eTransferDstOptimal);
-
-	// commandSubmitter.copyBufferToImage(stagingResource->object, resource->object, texWidth, texHeight);
+	commandSubmitter.transitionImageLayout(resourceImage, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
 
 	commandSubmitter.copyBufferToImage(stagingBuffer, resourceImage, texWidth, texHeight);
 
-	// commandSubmitter.transitionImageLayout(resource->object, vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eShaderReadOnlyOptimal);
-
-	commandSubmitter.transitionImageLayout(resourceImage, vk::ImageLayout::eTransferDstOptimal, vk::ImageLayout::eShaderReadOnlyOptimal);
+	commandSubmitter.transitionImageLayout(resourceImage, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 
 	vmaDestroyBuffer(allocator, stagingBuffer, stagingAllocation);
 
