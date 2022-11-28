f490e59624a403dc8f36322b0deee30e111095a8
voxelmesh <voxelmesh@proton.me>
2022-11-28 13:32:04 -0800

    Added new files for event handling in main

---
+++ include/vkx/events.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace vkx {
+namespace events {
+void quit();
+
+void windowEvent();
+
+void windowResized();
+
+void mouseMoved();
+
+void mouseButtonDown();
+
+void mouseButtonUp();
+
+void keyDown();
+
+void keyUp();
+} // namespace events
+} // namespace vkx
---
+++ src/events.cpp
@@ -0,0 +1,33 @@
+#include <vkx/events.hpp>
+
+void vkx::events::quit() {
+
+}
+
+void vkx::events::windowEvent() {
+
+}
+
+void vkx::events::windowResized() {
+
+}
+
+void vkx::events::mouseMoved() {
+
+}
+
+void vkx::events::mouseButtonDown() {
+
+}
+
+void vkx::events::mouseButtonUp() {
+
+}
+
+void vkx::events::keyDown() {
+
+}
+
+void vkx::events::keyUp() {
+
+}
