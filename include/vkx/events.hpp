ca6a505b1d824ac76d742b0a86e9d790d3f31541
voxelmesh <voxelmesh@proton.me>
2022-11-28 14:25:38 -0800

    Removed code for studying of event loop patterns

--- include/vkx/events.hpp
+++ include/vkx/events.hpp
@@ -2,20 +2,5 @@
 
 namespace vkx {
 namespace events {
-void quit();
-
-void windowEvent();
-
-void windowResized();
-
-void mouseMoved();
-
-void mouseButtonDown();
-
-void mouseButtonUp();
-
-void keyDown();
-
-void keyUp();
 } // namespace events
 } // namespace vkx
