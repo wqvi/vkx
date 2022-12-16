3ed6bf3ff0f45da00204e774acbd7b73ebd199a2
voxelmesh <voxelmesh@proton.me>
2022-12-16 13:35:35 -0800

    Removed old voxel chunk

--- include/vkx/voxels/voxels.hpp
+++ include/vkx/voxels/voxels.hpp
@@ -4,24 +4,7 @@
 #include <vkx/renderer/model.hpp>
 
 namespace vkx {
-template <class T>
-constexpr std::size_t flattenIndex(std::size_t size, std::size_t accumulator, T index) {
-	return accumulator * index;
-}
-
-template <class T, class... Y>
-constexpr std::size_t flattenIndex(std::size_t size, std::size_t accumulator, T x, Y... indices) {
-	const auto index = flattenIndex(size, accumulator * size, indices...);
-	return static_cast<std::size_t>(x) * accumulator + index;
-}
-
-template <class T, class... Y>
-constexpr std::size_t flattenIndex(std::size_t size, T x, Y... indices) {
-	const auto index = flattenIndex(size, size, indices...);
-	return static_cast<std::size_t>(x) + index;
-}
-
-enum class Voxel : std::uint32_t {
+enum class Voxel : std::int32_t {
 	Air,
 	Stone,
 	Dirt
@@ -34,57 +17,6 @@ struct VoxelMask {
 	bool operator==(const VoxelMask& other) const;
 
 	bool operator!=(const VoxelMask& other) const;
-};
-
-class VoxelChunk {
-private:
-	using Mask = std::vector<vkx::VoxelMask>;
-
-public:
-	std::int32_t size;
-
-	glm::ivec3 chunkPosition;
-	std::vector<Voxel> voxels;
-	std::vector<vkx::Vertex> vertices;
-	std::vector<std::uint32_t> indices;
-
-	std::uint32_t vertexCount;
-
-	std::vector<vkx::Vertex>::iterator vertexIter;
-	std::vector<std::uint32_t>::iterator indexIter;
-
-	glm::ivec3 normalizedPosition;
-
-	std::vector<vkx::VoxelMask> mask;
-	glm::ivec3 chunkItr;
-	glm::ivec3 axisMask;
-
-	VoxelChunk() = default;
-
-	explicit VoxelChunk(std::int32_t size, const glm::vec3& chunkPosition);
-
-	vkx::Voxel at(const glm::ivec3& position);
-
-	void set(const glm::ivec3& position, vkx::Voxel voxel);
-
-	void set(std::int32_t x, std::int32_t y, std::int32_t z, vkx::Voxel voxel);
-
-	void greedy();
-
-private:
-	bool validLocation(std::int32_t x, std::int32_t y, std::int32_t z);
-
-	void computeMask(std::int32_t axis1, std::int32_t axis2);
-
-	std::int32_t computeWidth(const vkx::VoxelMask& currentMask, std::int32_t i, std::int32_t n);
-
-	std::int32_t computeHeight(const vkx::VoxelMask& currentMask, std::int32_t j, std::int32_t n, std::int32_t width);
-
-	void clear(std::int32_t width, std::int32_t height, std::int32_t n);
-
-	void computeMesh(std::int32_t axis1, std::int32_t axis2);
-
-	void createQuad(std::int32_t normal, const glm::ivec3& axisMask, std::int32_t width, std::int32_t height, const glm::ivec3& pos, std::int32_t axis1, std::int32_t axis2);
 };
 
 struct VoxelVertex {
