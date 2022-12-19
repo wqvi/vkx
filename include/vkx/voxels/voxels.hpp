a16355be4f4828a0029fee668d50d5541f54fbdd
voxelmesh <voxelmesh@proton.me>
2022-12-19 09:46:07 -0800

    Moved voxel vertex to vertex.hpp

--- include/vkx/voxels/voxels.hpp
+++ include/vkx/voxels/voxels.hpp
@@ -19,34 +19,6 @@ struct VoxelMask {
 	bool operator!=(const VoxelMask& other) const;
 };
 
-struct VoxelVertex {
-	glm::vec2 pos;
-	glm::vec2 uv;
-	glm::vec2 normal;
-
-	VoxelVertex() = default;
-
-	explicit VoxelVertex(const glm::vec2& pos, const glm::vec2& uv, const glm::vec2& normal);
-
-	static auto getBindingDescription() noexcept {
-		std::vector<VkVertexInputBindingDescription> bindingDescriptions{};
-
-		bindingDescriptions.push_back({0, sizeof(VoxelVertex), VK_VERTEX_INPUT_RATE_VERTEX});
-
-		return bindingDescriptions;
-	}
-
-	static auto getAttributeDescriptions() noexcept {
-		std::vector<VkVertexInputAttributeDescription> attributeDescriptions{};
-
-		attributeDescriptions.push_back({0, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(VoxelVertex, pos)});
-		attributeDescriptions.push_back({1, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(VoxelVertex, uv)});
-		attributeDescriptions.push_back({2, 0, VK_FORMAT_R32G32_SFLOAT, offsetof(VoxelVertex, normal)});
-
-		return attributeDescriptions;
-	}
-};
-
 static constexpr std::size_t CHUNK_SIZE = 64;
 
 class HostHeapMesh {
