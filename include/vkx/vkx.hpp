b8958ad69b8c4e2e833b1f21da2d961dde8bb96c
voxelmesh <voxelmesh@proton.me>
2022-12-20 09:49:26 -0800

    Added window to vkx.hpp

--- include/vkx/vkx.hpp
+++ include/vkx/vkx.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <vkx/voxels/voxels.hpp>
-#include <vkx/renderer/uniform_buffer.hpp>
-#include <vkx/renderer/model.hpp>
 #include <vkx/camera.hpp>
 #include <vkx/raycast.hpp>
+#include <vkx/renderer/model.hpp>
 #include <vkx/renderer/renderer.hpp>
+#include <vkx/renderer/uniform_buffer.hpp>
+#include <vkx/voxels/voxels.hpp>
+#include <vkx/window.hpp>
