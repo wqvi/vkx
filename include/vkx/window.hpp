861d0c5d6c43e0ae2110682d761e0df0eb07f302
voxelmesh <voxelmesh@proton.me>
2022-12-20 09:49:00 -0800

    Window initializes SDL but only once

--- include/vkx/window.hpp
+++ include/vkx/window.hpp
@@ -2,6 +2,10 @@
 
 namespace vkx {
 class Window {
+private:
+	static std::once_flag initialized;
+	SDL_Window* windowHandle = nullptr;
+
 public:
 	Window() = default;
 
--- src/window.cpp
+++ src/window.cpp
@@ -1,1 +1,12 @@
 #include <vkx/window.hpp>
+
+std::once_flag vkx::Window::initialized = {};
+
+vkx::Window::Window(const char* name, std::int32_t width, std::int32_t height) {
+	std::call_once(
+	    initialized, [](auto flags) {
+		    if (SDL_Init(flags) != 0) {
+				throw std::runtime_error(SDL_GetError());
+			}
+		}, SDL_INIT_EVERYTHING);
+}
