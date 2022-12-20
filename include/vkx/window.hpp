9744afa5b522f07053199fb57f22f58532be701d
voxelmesh <voxelmesh@proton.me>
2022-12-20 10:22:52 -0800

    Fleshing out window

--- include/vkx/window.hpp
+++ include/vkx/window.hpp
@@ -10,5 +10,19 @@ public:
 	Window() = default;
 
 	explicit Window(const char* name, std::int32_t width, std::int32_t height);
+
+	Window(const Window& other) = delete;
+
+	Window(Window&& other) noexcept;
+
+	~Window();
+
+	Window& operator=(const Window& other) = delete;
+
+	Window& operator=(Window&& other) noexcept;
+
+	explicit operator SDL_Window*() const;
+
+	std::pair<std::int32_t, std::int32_t> getDimensions() const;
 };
 } // namespace vkx
--- src/window.cpp
+++ src/window.cpp
@@ -10,3 +10,31 @@ vkx::Window::Window(const char* name, std::int32_t width, std::int32_t height) {
 			}
 		}, SDL_INIT_EVERYTHING);
 }
+
+vkx::Window::Window(Window&& other) noexcept 
+	: windowHandle(std::move(other.windowHandle)) {
+	other.windowHandle = nullptr;
+}
+
+vkx::Window::~Window() {
+	if (windowHandle) {
+		SDL_DestroyWindow(windowHandle);
+	}
+}
+
+vkx::Window& vkx::Window::operator=(Window&& other) noexcept {
+	windowHandle = std::move(other.windowHandle);
+	other.windowHandle = nullptr;
+	return *this;
+}
+
+vkx::Window::operator SDL_Window*() const {
+	return windowHandle;
+}
+
+std::pair<std::int32_t, std::int32_t> vkx::Window::getDimensions() const {
+	int width = 0;
+	int height = 0;
+	SDL_Vulkan_GetDrawableSize(windowHandle, &width, &height);
+	return std::make_pair(width, height);
+}
