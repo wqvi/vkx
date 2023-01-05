e15ef9ba6135c08d9a98592e3a363261353fd604
voxelmesh <voxelmesh@proton.me>
2023-01-05 10:18:57 -0800

    Making camera more concise and readable

--- include/vkx/camera.hpp
+++ include/vkx/camera.hpp
@@ -13,47 +13,17 @@ namespace vkx {
 
 [[maybe_unused]] static constexpr glm::vec3 DOWN = glm::vec3{0.0f, -1.0f, 0.0f};
 
-class Camera {
-public:
-	Camera() = default;
-
-	explicit Camera(const glm::vec3& position);
-
-	template <class... T>
-	explicit Camera(T... t)
-	    : position(t...) {}
-
-	void updateMouse(const glm::vec2& relative);
-
-	void updateKey(SDL_Keycode key);
-
-	[[nodiscard]] glm::mat4 viewMatrix() const;
-
-	glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::quat yawOrientation = glm::quat(0.0f, 0.0f, 0.0f, 1.0f);
-	glm::quat pitchOrientation = glm::quat(0.0f, 0.0f, 0.0f, 1.0f);
-	glm::vec2 sensitivity = glm::vec2(0.1f, 0.1f);
-	union {
-		glm::vec2 rotation = glm::vec2(0.0f, 0.0f);
-		struct {
-			float pitch;
-			float yaw;
-		};
-	};
-	glm::vec3 front = glm::vec3(0.0f, 0.0f, 0.0f);
-	glm::vec3 direction = glm::vec3(0.0f, 0.0f, 0.0f);
-};
-
 class Camera2D {
 private:
 	glm::vec2 position{0, 0};
 
+	// This is deemed unsafe however it is a useful tool
+	// For calculating any sort of rotation related stuff
 	union {
-		glm::vec2 rotation{0, 0};
+		glm::vec2 rotationDegrees{0, 0};
 		struct {
-			float pitch;
-			float yaw;
+			float pitchDegrees;
+			float yawDegrees;
 		};
 	};
 
@@ -71,7 +41,7 @@ public:
 
 	glm::mat4 viewMatrix() const noexcept;
 
-	void rotate(const glm::vec2& rotation) noexcept;
+	void rotate(const glm::vec2& rotationDegrees) noexcept;
 
 	void move(const glm::vec2& offsetPosition) noexcept;
 
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,69 +1,9 @@
-#include <glm/ext/quaternion_geometric.hpp>
-#include <glm/trigonometric.hpp>
 #include <vkx/camera.hpp>
-
-#include <SDL2/SDL_keycode.h>
-
-vkx::Camera::Camera(const glm::vec3& position)
-    : position(position) {}
-
-void vkx::Camera::updateMouse(const glm::vec2& relative) {
-	pitch += relative.y * sensitivity.x;
-	yaw += relative.x * sensitivity.y;
-	pitch = glm::clamp(rotation.x, -89.0f, 89.0f);
-	yaw = glm::mod(rotation.y, 360.0f);
-
-	const auto pitchRadians = glm::radians(pitch);
-	const auto yawRadians = glm::radians(-yaw);
-
-	pitchOrientation = glm::angleAxis(pitchRadians, glm::vec3(1.0f, 0.0f, 0.0f));
-	yawOrientation = glm::angleAxis(yawRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-
-	const auto orientation = yawOrientation * pitchOrientation;
-	const auto quaternionFront = orientation * glm::quat(0.0f, 0.0f, 0.0f, -1.0f) * glm::conjugate(orientation);
-	front = glm::normalize(glm::vec3{quaternionFront.x, quaternionFront.y, -quaternionFront.z});
-}
-
-void vkx::Camera::updateKey(SDL_Keycode key) {
-	// FPS Camera
-	// const auto orientation = yawOrientation * glm::quat(0.0f, 0.0f, 0.0f, 1.0f);
-	// Free Camera
-	const auto orientation = yawOrientation * pitchOrientation;
-	const auto quaternionFront = orientation * glm::quat(0.0f, 0.0f, 0.0f, -1.0f) * glm::conjugate(orientation);
-	front = glm::normalize(glm::vec3{quaternionFront.x, quaternionFront.y, quaternionFront.z});
-	const glm::vec3 right = glm::normalize(glm::cross(front, vkx::UP));
-
-	// TODO fix this monstrosity
-	if (key == SDLK_w) {
-		direction = -front;
-		return;
-	}
-	if (key == SDLK_s) {
-		direction = front;
-		return;
-	}
-	if (key == SDLK_a) {
-		direction = -right;
-		return;
-	}
-	if (key == SDLK_d) {
-		direction = right;
-		return;
-	}
-
-	front.z = -front.z;
-}
-
-[[nodiscard]] glm::mat4 vkx::Camera::viewMatrix() const {
-	const auto viewRotation = glm::mat4_cast(glm::conjugate(yawOrientation * pitchOrientation));
-	const auto viewTranslation = glm::translate(glm::mat4(1.0f), -position);
-	return viewRotation * viewTranslation;
-}
 
 vkx::Camera2D::Camera2D(const glm::vec2& position,
 			const glm::vec2& rotation,
 			const glm::vec2& mouseSensitivity)
-    : position(position), rotation(rotation), mouseSensitivity(mouseSensitivity) {}
+    : position(position), rotationDegrees(rotation), mouseSensitivity(mouseSensitivity) {}
 
 glm::mat4 vkx::Camera2D::viewMatrix() const noexcept {
 	const auto viewRotation = glm::mat3_cast(glm::conjugate(yawOrientation * pitchOrientation));
@@ -71,17 +11,17 @@ glm::mat4 vkx::Camera2D::viewMatrix() const noexcept {
 	return viewRotation * viewTranslation;
 }
 
-void vkx::Camera2D::rotate(const glm::vec2& rotation) noexcept {
-	pitch += rotation.y * mouseSensitivity.x;
-	yaw += rotation.x * mouseSensitivity.y;
-	pitch = glm::clamp(pitch, -89.0f, 89.0f);
-	yaw = glm::mod(yaw, 360.0f);
+void vkx::Camera2D::rotate(const glm::vec2& rotationDegrees) noexcept {
+	pitchDegrees += rotationDegrees.y * mouseSensitivity.x;
+	yawDegrees += rotationDegrees.x * mouseSensitivity.y;
+	pitchDegrees = glm::clamp(pitchDegrees, -89.0f, 89.0f);
+	yawDegrees = glm::mod(yawDegrees, 360.0f);
 
-	const auto pitchRadians = glm::radians(pitch);
-	const auto yawRadians = glm::radians(-yaw);
+	const auto pitchRadians = glm::radians(pitchDegrees);
+	const auto yawRadians = -glm::radians(yawDegrees);
 
-	pitchOrientation = glm::angleAxis(pitchRadians, glm::vec3(1.0f, 0.0f, 0.0f));
-	yawOrientation = glm::angleAxis(yawRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+	pitchOrientation = glm::angleAxis(pitchRadians, vkx::EAST);
+	yawOrientation = glm::angleAxis(yawRadians, vkx::UP);
 }
 
 void vkx::Camera2D::move(const glm::vec2& offsetPosition) noexcept {
