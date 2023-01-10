646133f747a8aa09fa2f97ce89b459d0f5114d7a
voxelmesh <voxelmesh@proton.me>
2023-01-10 10:53:42 -0800

    Moved image to image header and source files from renderer.hpp

--- include/vkx/renderer/image.hpp
+++ include/vkx/renderer/image.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
-#include <vkx/renderer/core/commands.hpp>
 #include <vkx/renderer/renderer.hpp>
 
 namespace vkx {
+class Image {
+private:
+	vk::Device logicalDevice{};
+	vk::UniqueImage resourceImage{};
+	vkx::UniqueVulkanAllocation resourceAllocation{};
+
+public:
+	Image() = default;
+
+	explicit Image(vk::Device logicalDevice, vk::UniqueImage&& image, UniqueVulkanAllocation&& allocation);
+
+	vk::UniqueImageView createView(vk::Format format, vk::ImageAspectFlags aspectFlags) const;
+};
 } // namespace vkx
--- src/renderer/image.cpp
+++ src/renderer/image.cpp
@@ -1,1 +1,26 @@
 #include <vkx/renderer/image.hpp>
+
+vkx::Image::Image(vk::Device logicalDevice, vk::UniqueImage&& image, vkx::UniqueVulkanAllocation&& allocation)
+    : logicalDevice(logicalDevice),
+      resourceImage(std::move(image)),
+      resourceAllocation(std::move(allocation)) {
+}
+
+vk::UniqueImageView vkx::Image::createView(vk::Format format, vk::ImageAspectFlags aspectFlags) const {
+	const vk::ImageSubresourceRange subresourceRange{
+	    aspectFlags,
+	    0,
+	    1,
+	    0,
+	    1};
+
+	const vk::ImageViewCreateInfo imageViewCreateInfo{
+	    {},
+	    *resourceImage,
+	    vk::ImageViewType::e2D,
+	    format,
+	    {},
+	    subresourceRange};
+
+	return logicalDevice.createImageViewUnique(imageViewCreateInfo);
+}
