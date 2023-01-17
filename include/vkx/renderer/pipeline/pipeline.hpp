16e51849bd4a15baafc51eb94b42ea788d6d43c3
voxelmesh <voxelmesh@proton.me>
2023-01-17 09:29:15 -0800

    Added silly little function

--- include/vkx/renderer/pipeline/pipeline.hpp
+++ include/vkx/renderer/pipeline/pipeline.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
 namespace vkx {
+namespace pipeline {
 class VulkanPipeline {
 private:
 	vk::Device logicalDevice{};
 
 private:
-	[[nodiscard]] vk::UniqueShaderModule createShaderModule() const;
+	[[nodiscard]] vk::UniqueShaderModule createShaderModule(const std::string& filename) const;
 };
+} // namespace pipeline
 } // namespace vkx
--- src/renderer/pipeline/pipeline.cpp
+++ src/renderer/pipeline/pipeline.cpp
@@ -1,2 +1,23 @@
 #include <vkx/renderer/pipeline/pipeline.hpp>
 
+vk::UniqueShaderModule vkx::pipeline::VulkanPipeline::createShaderModule(const std::string& filename) const {
+	std::ifstream file{filename, std::ios::ate | std::ios::binary};
+
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to open file.");
+	}
+
+	const auto fileSize = static_cast<std::size_t>(file.tellg());
+	std::vector<char> buffer{};
+	buffer.resize(fileSize);
+
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+
+	const vk::ShaderModuleCreateInfo shaderModuleCreateInfo{
+	    {},
+	    static_cast<std::uint32_t>(buffer.size()),
+	    reinterpret_cast<const std::uint32_t*>(buffer.data())};
+
+	return logicalDevice.createShaderModuleUnique(shaderModuleCreateInfo);
+}
