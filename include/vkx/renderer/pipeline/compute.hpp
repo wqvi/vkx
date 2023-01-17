76d9d012471dcd82e09db6f3fc482a562239a0b3
voxelmesh <voxelmesh@proton.me>
2023-01-17 13:09:20 -0800

    Working on compute pipeline

--- include/vkx/renderer/pipeline/compute.hpp
+++ include/vkx/renderer/pipeline/compute.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <vkx/renderer/buffers.hpp>
+#include <vkx/renderer/pipeline/pipeline.hpp>
+
 namespace vkx {
-class ComputePipeline {
+namespace pipeline {
+struct ComputePipelineInformation {
+	const std::string file{};
+	const std::vector<vk::DescriptorSetLayoutBinding> bindings{};
+	const std::vector<vk::VertexInputBindingDescription> bindingDescriptions{};
+	const std::vector<vk::VertexInputAttributeDescription> attributeDescriptions{};
+	const std::vector<std::size_t> uniformSizes{};
+	const std::vector<const Texture*> textures;
+};
+
+class ComputePipeline : public vkx::pipeline::VulkanPipeline {
 private:
 	vk::Device logicalDevice{};
 	vk::UniqueDescriptorSetLayout descriptorLayout{};
@@ -13,6 +26,9 @@ private:
 public:
 	ComputePipeline() = default;
 
-
+	explicit ComputePipeline(vk::Device logicalDevice,
+				 const vkx::VulkanAllocator& allocator,
+				 const vkx::pipeline::ComputePipelineInformation& info);
 };
+} // namespace pipeline
 } // namespace vkx
--- src/renderer/pipeline/compute.cpp
+++ src/renderer/pipeline/compute.cpp
@@ -1,1 +1,6 @@
 #include <vkx/renderer/pipeline/compute.hpp>
+
+vkx::pipeline::ComputePipeline::ComputePipeline(vk::Device logicalDevice,
+						const vkx::VulkanAllocator& allocator,
+						const vkx::pipeline::ComputePipelineInformation& info) {
+}
